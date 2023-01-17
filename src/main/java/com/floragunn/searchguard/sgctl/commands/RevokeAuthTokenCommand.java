03c877975b60f1ab48ae3ec471a8aee01dac45d9
scosta <samuel.costa@eliatra.com>
2023-01-17 03:14:34 +0000

    Issue-42 Additional Test and removing API focused tests

--- src/main/java/com/floragunn/searchguard/sgctl/commands/RevokeAuthTokenCommand.java
+++ src/main/java/com/floragunn/searchguard/sgctl/commands/RevokeAuthTokenCommand.java
@@ -8,28 +8,24 @@ import picocli.CommandLine.Command;
 
 import java.util.concurrent.Callable;
 
-@Command(name = "revoke-auth-tokens", description = "Revoke specified auth token")
+@Command(name = "revoke-auth-token", description = "Revoke specified auth token")
 
-
-public class RevokeAuthTokensCommand extends ConnectingCommand  implements Callable<Integer> {
+public class RevokeAuthTokenCommand extends ConnectingCommand  implements Callable<Integer> {
 
     @Parameters(index =  "0", arity = "1", description = "The ID of the auth token to be removed")
     String authTokenId;
     @Override
     public Integer call() throws Exception {
 
-
         try (SearchGuardRestClient client = getClient().debug(debug)) {
             BasicResponse response = client.revokeAuthToken(authTokenId);
             System.out.println("Remove AuthToken Response: " + response);
-            return 0;
+            return CommandLine.ExitCode.OK;
 
         }
-        catch (NumberFormatException e) { //TODO Specific exception catching
-            System.out.println("error " + e);
-            return 1;
+        catch (Exception e) {
+            System.out.println("Exception encountered while trying to revoke the specified Auth token " + authTokenId + " :" +  e);
+            return CommandLine.ExitCode.SOFTWARE;
         }
-
-
     }
 }
