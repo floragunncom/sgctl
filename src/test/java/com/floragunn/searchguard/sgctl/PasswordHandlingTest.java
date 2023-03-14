427c71b62649b6ac6545a6183e16f01c2cb4e80b
Aki Abramowski <abraakie@web.de>
2023-03-14 11:06:53 +0100

    improve password handling tests

--- src/test/java/com/floragunn/searchguard/sgctl/PasswordHandlingTest.java
+++ src/test/java/com/floragunn/searchguard/sgctl/PasswordHandlingTest.java
@@ -1,25 +1,27 @@
 package com.floragunn.searchguard.sgctl;
 
+import com.floragunn.codova.documents.DocNode;
 import com.floragunn.codova.documents.DocReader;
+import com.floragunn.codova.documents.Format;
 import com.floragunn.searchguard.test.helper.certificate.TestCertificate;
 import com.floragunn.searchguard.test.helper.certificate.TestCertificates;
 import com.floragunn.searchguard.test.helper.cluster.LocalCluster;
-import com.google.common.collect.ImmutableList;
 import org.apache.commons.io.output.TeeOutputStream;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.IOException;
 import java.io.PrintStream;
 import java.net.InetSocketAddress;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 
 import static java.util.Collections.singletonList;
@@ -42,120 +44,164 @@ public class PasswordHandlingTest {
         System.setErr(standardErr);
     }
 
-    private final static TestCertificates TEST_CERTIFICATES_PRIVATE_KEY_PASS = TestCertificates.builder().ca("CN=root.ca.example.com,OU=SearchGuard,O=SearchGuard")
-            .addNodes("CN=127.0.0.1,OU=SearchGuard,O=SearchGuard")
-            .addAdminClients(singletonList("CN=admin-0.example.com,OU=SearchGuard,O=SearchGuard"), 10, "secret").build();
-
-    private final static TestCertificates TEST_CERTIFICATES_NO_PRIVAT_KEY_PASS = TestCertificates.builder().ca("CN=root.ca.example.com,OU=SearchGuard,O=SearchGuard")
-            .addNodes("CN=127.0.0.1,OU=SearchGuard,O=SearchGuard")
-            .addAdminClients(singletonList("CN=admin-0.example.com,OU=SearchGuard,O=SearchGuard"), 10, null).build();
-
     private static LocalCluster cluster;
     private static Path configDir;
+    private InetSocketAddress httpAddress;
+    private String adminCert;
+    private String adminKey;
+    private String rootCaCert;
 
-    void prepareCluster(boolean keyPass, boolean saveKeyPass) throws IOException {
-        cluster = new LocalCluster.Builder().singleNode().sslEnabled(keyPass ? TEST_CERTIFICATES_PRIVATE_KEY_PASS : TEST_CERTIFICATES_NO_PRIVAT_KEY_PASS).start();
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    class PrivateKeyPassword {
+        private final TestCertificates testCertificates = TestCertificates.builder().ca("CN=root.ca.example.com,OU=SearchGuard,O=SearchGuard")
+                .addNodes("CN=127.0.0.1,OU=SearchGuard,O=SearchGuard")
+                .addAdminClients(singletonList("CN=admin-0.example.com,OU=SearchGuard,O=SearchGuard"), 10, "secret").build();
 
-        InetSocketAddress httpAddress = cluster.getHttpAddress();
-        TestCertificate adminCertificate = cluster.getTestCertificates().getAdminCertificate();
-        String adminCert = adminCertificate.getCertificateFile().getPath();
-        String adminKey = adminCertificate.getPrivateKeyFile().getPath();
-        String rootCaCert = cluster.getTestCertificates().getCaCertFile().getPath();
-        configDir = Files.createTempDirectory("sgctl-test-config");
+        @BeforeAll
+        public void setupCluster() {
+            cluster = new LocalCluster.Builder().singleNode().sslEnabled(testCertificates).start();
 
-        List<String> args = keyPass ? ImmutableList.of(
-                "connect", "--debug",
-                "-h", httpAddress.getHostString(),
-                "-p", String.valueOf(httpAddress.getPort()),
-                "--cert", adminCert,
-                "--key", adminKey,
-                "--ca-cert", rootCaCert,
-                "--sgctl-config-dir", configDir.toString(),
-                "--save-key-pass", Boolean.toString(saveKeyPass),
-                "--key-pass", "secret"
-        ) : ImmutableList.of(
-                "connect", "--debug",
-                "-h", httpAddress.getHostString(),
-                "-p", String.valueOf(httpAddress.getPort()),
-                "--cert", adminCert,
-                "--key", adminKey,
-                "--ca-cert", rootCaCert,
-                "--sgctl-config-dir", configDir.toString(),
-                "--save-key-pass", Boolean.toString(saveKeyPass)
-        );
-        int rc = SgctlTool.exec(args.toArray(new String[0]));
-        Assertions.assertEquals(0, rc);
+            httpAddress = cluster.getHttpAddress();
+            TestCertificate adminCertificate = cluster.getTestCertificates().getAdminCertificate();
+            adminCert = adminCertificate.getCertificateFile().getPath();
+            adminKey = adminCertificate.getPrivateKeyFile().getPath();
+            rootCaCert = cluster.getTestCertificates().getCaCertFile().getPath();
+        }
+
+        @AfterAll
+        public void destroy() {
+            cluster.close();
+        }
+
+        @Test
+        public void testSave() throws Exception {
+            configDir = Files.createTempDirectory("sgctl-test-config");
+            int result = SgctlTool.exec("connect", "--debug",
+                    "-h", httpAddress.getHostString(),
+                    "-p", String.valueOf(httpAddress.getPort()),
+                    "--cert", adminCert,
+                    "--key", adminKey,
+                    "--ca-cert", rootCaCert,
+                    "--sgctl-config-dir", configDir.toString(),
+                    "--save-key-pass", "true",
+                    "--key-pass", "secret");
+            Assertions.assertEquals(0, result);
+
+            Path sgConfigDestinationDir = Files.createTempDirectory("sgctl-test-sgconfig");
+            result = SgctlTool.exec("get-config", "-o", sgConfigDestinationDir.toString(), "--debug", "--sgctl-config-dir", configDir.toString());
+            Assertions.assertEquals(0, result);
+
+            result = SgctlTool.exec("get-config", "-o", sgConfigDestinationDir.toString(), "--debug", "--sgctl-config-dir", configDir.toString(), "--key-pass", "s");
+            Assertions.assertEquals(1, result);
+
+            Assertions.assertTrue(configContainsKeyPass());
+            Assertions.assertEquals("secret", getConfigKeyPass());
+        }
+
+        @Test
+        public void testNoSave() throws Exception {
+            configDir = Files.createTempDirectory("sgctl-test-config");
+            int result = SgctlTool.exec("connect", "--debug",
+                    "-h", httpAddress.getHostString(),
+                    "-p", String.valueOf(httpAddress.getPort()),
+                    "--cert", adminCert,
+                    "--key", adminKey,
+                    "--ca-cert", rootCaCert,
+                    "--sgctl-config-dir", configDir.toString(),
+                    "--save-key-pass", "false",
+                    "--key-pass", "secret");
+            Assertions.assertEquals(0, result);
+
+            Path sgConfigDestinationDir = Files.createTempDirectory("sgctl-test-sgconfig");
+            result = SgctlTool.exec("get-config", "-o", sgConfigDestinationDir.toString(), "--debug", "--sgctl-config-dir", configDir.toString());
+            Assertions.assertEquals(1, result);
+
+            result = SgctlTool.exec("get-config", "-o", sgConfigDestinationDir.toString(), "--debug", "--sgctl-config-dir", configDir.toString(), "--key-pass", "s");
+            Assertions.assertEquals(1, result);
+
+            result = SgctlTool.exec("get-config", "-o", sgConfigDestinationDir.toString(), "--debug", "--sgctl-config-dir", configDir.toString(), "--key-pass", "secret");
+            Assertions.assertEquals(0, result);
+
+            Assertions.assertFalse(configContainsKeyPass());
+            Assertions.assertNull(getConfigKeyPass());
+        }
     }
 
-    @AfterEach
-    public void destroy() throws Exception {
-        cluster.close();
-    }
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    class NoPrivateKeyPassword {
+        private final TestCertificates testCertificates = TestCertificates.builder().ca("CN=root.ca.example.com,OU=SearchGuard,O=SearchGuard")
+                .addNodes("CN=127.0.0.1,OU=SearchGuard,O=SearchGuard")
+                .addAdminClients(singletonList("CN=admin-0.example.com,OU=SearchGuard,O=SearchGuard"), 10, null).build();
 
-    @Test
-    public void testSaveKeyPass() throws Exception {
-        prepareCluster(true, true);
+        @BeforeAll
+        public void setupCluster() {
+            cluster = new LocalCluster.Builder().singleNode().sslEnabled(testCertificates).start();
 
-        Path sgConfigDestinationDir = Files.createTempDirectory("sgctl-test-sgconfig");
-        int result = SgctlTool.exec("get-config", "-o", sgConfigDestinationDir.toString(), "--debug", "--sgctl-config-dir", configDir.toString());
-        Assertions.assertEquals(0, result);
+            httpAddress = cluster.getHttpAddress();
+            TestCertificate adminCertificate = cluster.getTestCertificates().getAdminCertificate();
+            adminCert = adminCertificate.getCertificateFile().getPath();
+            adminKey = adminCertificate.getPrivateKeyFile().getPath();
+            rootCaCert = cluster.getTestCertificates().getCaCertFile().getPath();
+        }
 
-        result = SgctlTool.exec("get-config", "-o", sgConfigDestinationDir.toString(), "--debug", "--sgctl-config-dir", configDir.toString(), "--key-pass", "s");
-        Assertions.assertEquals(1, result);
+        @AfterAll
+        public void destroy() {
+            cluster.close();
+        }
 
-        Assertions.assertTrue(configContainsKeyPass());
-        Assertions.assertEquals("secret", getConfigKeyPass());
-    }
+        @Test
+        public void testSave() throws Exception {
+            configDir = Files.createTempDirectory("sgctl-test-config");
+            int result = SgctlTool.exec("connect", "--debug",
+                    "-h", httpAddress.getHostString(),
+                    "-p", String.valueOf(httpAddress.getPort()),
+                    "--cert", adminCert,
+                    "--key", adminKey,
+                    "--ca-cert", rootCaCert,
+                    "--sgctl-config-dir", configDir.toString(),
+                    "--save-key-pass", "true");
+            Assertions.assertEquals(0, result);
 
-    @Test
-    public void testSaveNoKeyPass() throws Exception {
-        prepareCluster(false, true);
+            Path sgConfigDestinationDir = Files.createTempDirectory("sgctl-test-sgconfig");
+            result = SgctlTool.exec("get-config", "-o", sgConfigDestinationDir.toString(), "--debug", "--sgctl-config-dir", configDir.toString());
+            Assertions.assertEquals(0, result);
 
-        Path sgConfigDestinationDir = Files.createTempDirectory("sgctl-test-sgconfig");
-        int result = SgctlTool.exec("get-config", "-o", sgConfigDestinationDir.toString(), "--debug", "--sgctl-config-dir", configDir.toString());
-        Assertions.assertEquals(0, result);
+            result = SgctlTool.exec("get-config", "-o", sgConfigDestinationDir.toString(), "--debug", "--sgctl-config-dir", configDir.toString(), "--key-pass", "s");
+            Assertions.assertEquals(0, result);
 
-        result = SgctlTool.exec("get-config", "-o", sgConfigDestinationDir.toString(), "--debug", "--sgctl-config-dir", configDir.toString(), "--key-pass", "s");
-        Assertions.assertEquals(0, result);
+            Assertions.assertTrue(configContainsKeyPass());
+            Assertions.assertNull(getConfigKeyPass());
+        }
 
-        Assertions.assertTrue(configContainsKeyPass());
-        Assertions.assertNull(getConfigKeyPass());
-    }
 
-    @Test
-    public void testNoSaveKeyPass() throws Exception {
-        prepareCluster(true, false);
+        @Test
+        public void testNoSave() throws Exception {
+            configDir = Files.createTempDirectory("sgctl-test-config");
+            int result = SgctlTool.exec("connect", "--debug",
+                    "-h", httpAddress.getHostString(),
+                    "-p", String.valueOf(httpAddress.getPort()),
+                    "--cert", adminCert,
+                    "--key", adminKey,
+                    "--ca-cert", rootCaCert,
+                    "--sgctl-config-dir", configDir.toString(),
+                    "--save-key-pass", "false");
+            Assertions.assertEquals(0, result);
 
-        Path sgConfigDestinationDir = Files.createTempDirectory("sgctl-test-sgconfig");
-        int result = SgctlTool.exec("get-config", "-o", sgConfigDestinationDir.toString(), "--debug", "--sgctl-config-dir", configDir.toString());
-        Assertions.assertEquals(1, result);
+            Path sgConfigDestinationDir = Files.createTempDirectory("sgctl-test-sgconfig");
+            result = SgctlTool.exec("get-config", "-o", sgConfigDestinationDir.toString(), "--debug", "--sgctl-config-dir", configDir.toString());
+            Assertions.assertEquals(0, result);
 
-        result = SgctlTool.exec("get-config", "-o", sgConfigDestinationDir.toString(), "--debug", "--sgctl-config-dir", configDir.toString(), "--key-pass", "s");
-        Assertions.assertEquals(1, result);
+            result = SgctlTool.exec("get-config", "-o", sgConfigDestinationDir.toString(), "--debug", "--sgctl-config-dir", configDir.toString(), "--key-pass", "s");
+            Assertions.assertEquals(0, result);
 
-        result = SgctlTool.exec("get-config", "-o", sgConfigDestinationDir.toString(), "--debug", "--sgctl-config-dir", configDir.toString(), "--key-pass", "secret");
-        Assertions.assertEquals(0, result);
+            result = SgctlTool.exec("get-config", "-o", sgConfigDestinationDir.toString(), "--debug", "--sgctl-config-dir", configDir.toString(), "--key-pass", "secret");
+            Assertions.assertEquals(0, result);
 
-        Assertions.assertFalse(configContainsKeyPass());
-        Assertions.assertNull(getConfigKeyPass());
-    }
-
-    @Test
-    public void testNoSaveNoKeyPass() throws Exception {
-        prepareCluster(false, false);
-
-        Path sgConfigDestinationDir = Files.createTempDirectory("sgctl-test-sgconfig");
-        int result = SgctlTool.exec("get-config", "-o", sgConfigDestinationDir.toString(), "--debug", "--sgctl-config-dir", configDir.toString());
-        Assertions.assertEquals(0, result);
-
-        result = SgctlTool.exec("get-config", "-o", sgConfigDestinationDir.toString(), "--debug", "--sgctl-config-dir", configDir.toString(), "--key-pass", "s");
-        Assertions.assertEquals(0, result);
-
-        result = SgctlTool.exec("get-config", "-o", sgConfigDestinationDir.toString(), "--debug", "--sgctl-config-dir", configDir.toString(), "--key-pass", "secret");
-        Assertions.assertEquals(0, result);
-
-        Assertions.assertTrue(configContainsKeyPass());
-        Assertions.assertNull(getConfigKeyPass());
+            Assertions.assertTrue(configContainsKeyPass());
+            Assertions.assertNull(getConfigKeyPass());
+        }
     }
 
     boolean configContainsKeyPass() throws Exception {
@@ -168,9 +214,6 @@ public class PasswordHandlingTest {
 
     String getConfigKeyPass() throws Exception {
         File clusterConfigFile = new File(configDir.toFile(), "cluster_" + cluster.getHttpAddress().getHostString() + ".yml");
-
-        Map<String, Object> config = DocReader.yaml().readObject(clusterConfigFile);
-        Map<String, Object> clientAuthConfig = (Map<String, Object>) ((Map<String, Object>) config.get("tls")).get("client_auth");
-        return (String) clientAuthConfig.get("private_key_password");
+        return (String) DocNode.parse(Format.YAML).from(clusterConfigFile).get("tls", "client_auth", "private_key_password");
     }
 }
