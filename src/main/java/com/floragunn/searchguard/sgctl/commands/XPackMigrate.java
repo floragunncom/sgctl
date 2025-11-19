fdf3e98ff90390f689bb4b00b67e44b9993f5a4b
Justus Laube <1717-jlaube@users.noreply.git.se.uni-hannover.de>
2025-11-19 20:27:39 +0100

    improved config parsing structure

--- src/main/java/com/floragunn/searchguard/sgctl/commands/XPackMigrate.java
+++ src/main/java/com/floragunn/searchguard/sgctl/commands/XPackMigrate.java
@@ -1,6 +1,10 @@
 package com.floragunn.searchguard.sgctl.commands;
 
+import com.floragunn.codova.documents.DocReader;
+import com.floragunn.codova.documents.DocumentParseException;
+import com.floragunn.codova.documents.UnexpectedDocumentStructureException;
 import com.floragunn.searchguard.sgctl.SgctlException;
+import com.floragunn.searchguard.sgctl.config.xpack.Role;
 import picocli.CommandLine;
 
 import java.io.IOException;
@@ -10,6 +14,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
+import java.util.function.Function;
 
 @CommandLine.Command(name = "migrate-security", description = "Converts X-Pack configs to Search Guard configs")
 public class XPackMigrate implements Callable<Integer> {
@@ -20,7 +25,9 @@ public class XPackMigrate implements Callable<Integer> {
     @CommandLine.Option(names = { "-o", "--output-dir" }, description = "Directory where to write new sg configuration files", required = true)
     Path outputDir;
 
-    private static final List<String> configNames = List.of("elasticsearch.yml", "roles.json", "users.json");
+    private static final Map<String, Function<Map<String, Object>, Record>> configParsers = Map.of(
+            // TODO: Add parsing functions here <filename>,Record::parse
+    );
 
     public Integer call() throws Exception {
         if (!Files.isDirectory(inputDir)) {
@@ -29,10 +36,9 @@ public class XPackMigrate implements Callable<Integer> {
         }
         System.out.println("Welcome to the Search Guard X-pack security migration tool.\n\n");
         try {
-            final var configStrings = readConfigStrings();
-            System.out.println(configStrings);
+            final var configs = parseConfigs();
+            System.out.println(configs);
             // TODO: More
-
 
         } catch (SgctlException e) {
             System.err.println("Error: " + e.getMessage());
@@ -41,18 +47,33 @@ public class XPackMigrate implements Callable<Integer> {
         return 0;
     }
 
-    private Map<String, String> readConfigStrings() throws SgctlException, IOException, IllegalArgumentException {
-        final Map<String, String> configs = new HashMap<>();
-        for (String configFileName : configNames) {
+    private Map<String, Record> parseConfigs()
+            throws SgctlException, IOException, DocumentParseException, UnexpectedDocumentStructureException, IllegalArgumentException {
+        final Map<String, Record> configs = new HashMap<>();
+        for (final var entry : configParsers.entrySet()) {
+            final var configFileName = entry.getKey();
+            final var parserFunction = entry.getValue();
+            // Ensure valid path
             final var configPath = inputDir.resolve(configFileName);
-            if (Files.isReadable(configPath)) {
-                if (Files.isDirectory(configPath)) {
-                    throw new SgctlException("Config is a directory, but should be a file: " + configFileName);
-                }
-
-                final String content = Files.readString(configPath);
-                configs.put(configFileName, content);
+            if (!Files.isReadable(configPath)) {
+                continue;
             }
+            if (Files.isDirectory(configPath)) {
+                throw new SgctlException("Config is a directory, but should be a file: " + configFileName);
+            }
+            // Read to Object
+            final Map<String, Object> config;
+            if (configFileName.endsWith(".yaml") || configFileName.endsWith(".yml")) {
+                config = DocReader.yaml().readObject(configPath.toFile());
+            } else if (configFileName.endsWith(".json")) {
+                config = DocReader.json().readObject(configPath.toFile());
+            } else {
+                throw new SgctlException("Invalid config file extension: " + configFileName);
+            }
+
+            // Parse config
+            final Record parsed = parserFunction.apply(config);
+            configs.put(configFileName, parsed);
         }
         return configs;
     }
