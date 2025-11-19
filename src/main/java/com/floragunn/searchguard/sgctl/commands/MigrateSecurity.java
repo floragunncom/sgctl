2ca717221934d20f4b170b9d70f02620a0182e3a
Etienne <ben.etienne.altmann@stud.uni-hannover.de>
2025-11-19 17:05:31 +0100

    Basic input path check to see if it's valid

--- src/main/java/com/floragunn/searchguard/sgctl/commands/MigrateSecurity.java
+++ src/main/java/com/floragunn/searchguard/sgctl/commands/MigrateSecurity.java
@@ -5,20 +5,26 @@ import picocli.CommandLine.Option;
 import picocli.CommandLine.Command;
 
 import java.io.File;
-import java.util.List;
 import java.util.concurrent.Callable;
 
-@Command(name = "migrate-security", description = "Converts Xpack configs to search guard config files with a given input.")
+@Command(name = "migrate-security", description = "Converts Xpack config files to search guard config files, also prints a report based on the conversions success.")
 public class MigrateSecurity implements Callable<Integer> {
 
-    @Parameters
-    List<String> parameters;
+    @Parameters(description = "Directory with all important Files, such as elasticsearch.yml")
+    File inputPath;
 
     @Option(names = { "-o", "--output-dir" }, description = "Directory where to write new configuration files")
     File outputDir;
 
     @Override
     public Integer call() throws Exception {
+        if (inputPath == null || !inputPath.exists()) {
+            System.err.println(
+                    "You must specify a valid directory to the xpack configuration files elasticsearch.yml and security.index on the command line");
+            return 1;
+        }
+
+        System.out.println("Completed Command");
         return 0;
     }
 }
