be36e2ed116d8760e35800624d1ad9be75491a0c
TorkeTechnologies <support@torketechnologies.com>
2025-11-19 18:06:30 +0100

    Added function stubs for possibly relevant .security index files.

--- src/main/java/com/floragunn/searchguard/sgctl/util/mapping/XpackConfigReader.java
+++ src/main/java/com/floragunn/searchguard/sgctl/util/mapping/XpackConfigReader.java
@@ -1,4 +1,17 @@
 package com.floragunn.searchguard.sgctl.util.mapping;
 
+import java.io.File;
+
 public class XpackConfigReader {
+    private void readUsers(File users) {
+
+    }
+
+    private void readRoles(File roles) {
+
+    }
+
+    private void readUserMappings(File userMappings) {
+
+    }
 }
