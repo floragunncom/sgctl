699cc62a4d8c3547f36b09edcec7882b053c9ba7
TorkeTechnologies <support@torketechnologies.com>
2025-11-21 13:47:32 +0100

    Basic DocReader for users.json.

--- src/main/java/com/floragunn/searchguard/sgctl/commands/MigrateSecurity.java
+++ src/main/java/com/floragunn/searchguard/sgctl/commands/MigrateSecurity.java
@@ -1,24 +1,42 @@
 package com.floragunn.searchguard.sgctl.commands;
 
+import com.floragunn.searchguard.sgctl.util.mapping.XPackConfigReader;
 import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Command;
 
 import java.io.File;
-import java.util.List;
 import java.util.concurrent.Callable;
 
 @Command(name = "migrate-security", description = "Converts Xpack configs to search guard config files with a given input.")
 public class MigrateSecurity implements Callable<Integer> {
 
     @Parameters
-    List<String> parameters;
+    File inputDir;
 
     @Option(names = { "-o", "--output-dir" }, description = "Directory where to write new configuration files")
     File outputDir;
 
     @Override
     public Integer call() throws Exception {
+
+        File user = null;
+        File role = null;
+        File userMapping = null;
+
+        for (File file : inputDir.listFiles()) {
+            switch(file.getName()) {
+            case "user.json":
+                user = file;
+            case "role.json":
+                role = file;
+            case "user_mapping.json":
+                userMapping = file;
+            }
+        }
+
+        var reader = new XPackConfigReader(null, user, role, userMapping);
+        reader.generateIR();
         return 0;
     }
 }
