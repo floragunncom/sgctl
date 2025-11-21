719fdc21f0d00acd88f9b800b805eb814a053c4e
Thore <thore.klattenberg@stud.uni-hannover.de>
2025-11-21 11:00:24 +0100

    sketch of how a reader could be

---
+++ src/main/java/com/floragunn/searchguard/sgctl/util/mapping/ConfigReader.java
@@ -0,0 +1,9 @@
+package com.floragunn.searchguard.sgctl.util.mapping;
+
+import java.util.Map;
+
+public interface ConfigReader {
+
+    // read a config and return its contents as a map
+    Map<String, Object> read() throws Exception;
+}
