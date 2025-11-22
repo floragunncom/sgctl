eb85e40b2e0f6ab0024551acf17ec86010e9cc72
TorkeTechnologies <support@torketechnologies.com>
2025-11-22 20:10:34 +0100

    Consolidated IntermediateRepresentation files because one was written wrong.

--- src/main/java/com/floragunn/searchguard/sgctl/util/mapping/ir/IntermediateRepresentation.java
+++ src/main/java/com/floragunn/searchguard/sgctl/util/mapping/ir/IntermediateRepresentation.java
@@ -6,13 +6,17 @@ import java.util.List;
 public class IntermediateRepresentation {
     List<User> users = new ArrayList<>();
     List<Role> roles = new ArrayList<>();
+    List<RoleMapping> roleMappings = new ArrayList<RoleMapping>();
 
     // Getter-Methods
     public void addUser(User user) { users.add(user); }
     public void addRole(Role role) { roles.add(role); }
+    public void addRoleMapping(RoleMapping roleMapping) { roleMappings.add(roleMapping); }
 
     // Setter-Methods
     public List<User> getUsers() { return users; }
     public List<Role> getRoles() { return roles; }
+    public List<RoleMapping> getRoleMappings() { return roleMappings; }
+
 
 }
