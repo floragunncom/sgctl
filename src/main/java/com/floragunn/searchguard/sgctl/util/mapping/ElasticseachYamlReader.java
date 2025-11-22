7963c134731eb7f07585f1d706a56e0feb400d6a
Thore <thore.klattenberg@stud.uni-hannover.de>
2025-11-22 19:05:46 +0100

    added a method in the configReader class that flattens the parsed map, but keeps all namespaces -> now we can check each key against a known option name of the xpack docs

--- src/main/java/com/floragunn/searchguard/sgctl/util/mapping/ElasticseachYamlReader.java
+++ src/main/java/com/floragunn/searchguard/sgctl/util/mapping/ElasticseachYamlReader.java
@@ -3,22 +3,15 @@ package com.floragunn.searchguard.sgctl.util.mapping;
 import com.floragunn.codova.documents.DocReader;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.Map;
 
-public class ElasticseachYamlReader implements ConfigReader {
-
-    private final File configFile;
+public class ElasticseachYamlReader extends ConfigReader {
 
     public ElasticseachYamlReader(File configFile) {
-        this.configFile = configFile;
+        super(configFile);
     }
 
-    @Override
-    @SuppressWarnings("unchecked")
-    public Map<String, Object> read() throws Exception {
-        // the read method returns indeed a Map
-        return (Map<String, Object>) DocReader.yaml().read(configFile);
-    }
 
     @Override
     public String toString() {
