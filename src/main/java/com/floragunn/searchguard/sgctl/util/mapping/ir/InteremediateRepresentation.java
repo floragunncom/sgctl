7308fe7dcef6804107bd637647ad06a8ec1bc03c
ramon.tebbe <ramon.tebbe@stud.uni-hannover.de>
2025-11-22 18:25:18 +0100

    added ir.addRoleMapping and ir.getRoleMapping

--- src/main/java/com/floragunn/searchguard/sgctl/util/mapping/ir/InteremediateRepresentation.java
+++ src/main/java/com/floragunn/searchguard/sgctl/util/mapping/ir/InteremediateRepresentation.java
@@ -5,7 +5,11 @@ import java.util.List;
 
 public class InteremediateRepresentation {
     List<User> users = new ArrayList<User>();
+    List<RoleMapping> roleMappings = new ArrayList<RoleMapping>();
 
-    public void  addUser(User user) { users.add(user); }
+    public void addUser(User user) { users.add(user); }
     public List<User> getUsers() { return users; }
+
+    public void addRoleMapping(RoleMapping roleMapping) { roleMappings.add(roleMapping); }
+    public List<RoleMapping> getRoleMappings() { return roleMappings; }
 }
