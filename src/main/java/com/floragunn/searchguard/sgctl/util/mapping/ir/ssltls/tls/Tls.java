08ae093d32f5ef513dd6663be790e3a568ae2659
Thore <thore.klattenberg@stud.uni-hannover.de>
2025-11-25 16:28:18 +0100

    updated the tls ir class that should now have all needed fields

--- src/main/java/com/floragunn/searchguard/sgctl/util/mapping/ir/ssltls/tls/Tls.java
+++ src/main/java/com/floragunn/searchguard/sgctl/util/mapping/ir/ssltls/tls/Tls.java
@@ -1,8 +1,32 @@
 package com.floragunn.searchguard.sgctl.util.mapping.ir.ssltls.tls;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tls {
-    boolean enabled;
-    String keystore_path;
-    String truststore_path;
-    boolean clientAuth;
+    boolean enabled; // activation of TLS
+
+    // Keystore
+    String keystorePath; // contains certificate and private key
+    String keystoreType; // format of the keystore
+    String keystorePassword; // pw of keystore -> NO PLAINTEXT!
+
+    // Truststore
+    String truststorePath;
+    String truststoreType;
+    String truststorePassword;
+
+    // PEM -> can be used instead of keystores
+    String certificatePath;
+    String privateKeyPath;
+    List<String> certificateAuthorities = new ArrayList<>(); // paths
+
+    // TLS
+    String verificationMode; // full, certificate, or none
+    boolean clientAuth; // whether clients must present a client certificate
+    boolean hostnameVerificationEnabled; // whether hostname in certificates must match the node name
+
+    // Constraints
+    List<String> supportedProtocols = new ArrayList<>();
+    List<String> ciphers = new ArrayList<>();
 }
