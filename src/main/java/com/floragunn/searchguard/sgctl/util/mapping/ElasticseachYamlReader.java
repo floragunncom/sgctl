7dd81ee8f8144e7502c7441da97f49af7dab707d
Thore <thore.klattenberg@stud.uni-hannover.de>
2025-11-25 23:22:47 +0100

    merge ConfigReader and ElasticseachYamlReader

--- src/main/java/com/floragunn/searchguard/sgctl/util/mapping/ElasticseachYamlReader.java
+++ src/main/java/com/floragunn/searchguard/sgctl/util/mapping/ElasticseachYamlReader.java
@@ -1,17 +1,59 @@
 package com.floragunn.searchguard.sgctl.util.mapping;
 
+import com.floragunn.codova.documents.DocReader;
 import com.floragunn.searchguard.sgctl.util.mapping.ir.IntermediateRepresentation;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.Map;
 
-public class ElasticseachYamlReader extends ConfigReader {
+public class ElasticseachYamlReader {
+
+    private final File configFile;
+    protected IntermediateRepresentation ir;
+    Map<String, Object> flattenedMap;
 
     public ElasticseachYamlReader(File configFile, IntermediateRepresentation ir) {
-        super(configFile, ir);
+        this.configFile = configFile;
+        this.ir = ir;
+        try {
+            Map<String, Object> map = read();
+            flattenedMap = flattenMap(map);
+            toIR(flattenedMap);
+            System.out.println(flattenedMap);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 
-    public void toIR(Map<String, Object> map) {
+    // read a config and return its contents as a map
+    @SuppressWarnings("unchecked")
+    public Map<String, Object> read() throws Exception {
+        // the read method returns indeed a Map
+        return (Map<String, Object>) DocReader.yaml().read(configFile);
+    }
+
+    // flatten the parsed map, but keep all namespaces
+    private Map<String, Object> flattenMap(Map<String, Object> map) {
+        Map<String, Object> result = new HashMap<>();
+        flattenMapRec(map, result, "");
+        return result;
+    }
+
+    private void flattenMapRec(Object value, Map<String, Object> result, String prefix) {
+        if (value instanceof Map) {
+            Map<?, ?> map = (Map<?, ?>) value;
+            map.forEach((k, v) -> {
+                String newKey = prefix.isEmpty() ? k.toString() : prefix + "." + k;
+                flattenMapRec(v, result, newKey);
+            });
+        } else {
+            result.put(prefix, value);
+        }
+    }
+
+    private void toIR(Map<String, Object> map) {
 
         String transportPrefix = "xpack.security.transport.ssl.";
         String httpPrefix = "xpack.security.http.ssl.";
