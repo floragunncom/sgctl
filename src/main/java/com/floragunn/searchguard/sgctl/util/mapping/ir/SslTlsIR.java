807055a9d5549e36b9061d0516a3f464c596da38
Ole Eberhardt <xxxxoexx@gmail.com>
2025-11-26 12:53:02 +0100

    Moved profileAllowedIPs and profileDeniedIPs to class SslTlsIR

--- src/main/java/com/floragunn/searchguard/sgctl/util/mapping/ir/SslTlsIR.java
+++ src/main/java/com/floragunn/searchguard/sgctl/util/mapping/ir/SslTlsIR.java
@@ -1,12 +1,66 @@
 package com.floragunn.searchguard.sgctl.util.mapping.ir;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SslTlsIR {
 
     public Tls transport;
     public Tls http;
 
+
+    Map<String, List<String>> profileAllowedIPs = new HashMap<>(); // transport only: List of IP addresses (value) to allow for this profile (key)
+    Map<String, List<String>> profileDeniedIPs = new HashMap<>(); // transport only: List of IP addresses (value) to deny for this profile (key)
+
     public SslTlsIR() {
         transport = new Tls();
         http = new Tls();
+    }
+
+    public void handleOptions(String optionName, Object optionValue) {
+        boolean error = false;
+
+        if (IntermediateRepresentation.assertType(optionValue, List.class)) {
+            List<?> value = (List<?>) optionValue;
+
+            if (value.isEmpty()) {
+                return;
+            }
+
+            // Regex for profileAllowedIPs/profileDeniedIPs with optionName profiles.$PROFILE.xpack.security.filter. allow or deny
+            Pattern profileIPsPattern = Pattern.compile("^profiles\\.([^.]+)\\.xpack\\.security\\.filter\\.(allow|deny)$");
+            Matcher mProfileIPs = profileIPsPattern.matcher(optionName);
+
+            if (!(value.get(0) instanceof String)) {
+                error = true;
+            } else if(mProfileIPs.find()) {
+                String profile = mProfileIPs.group(1);
+                String action  = mProfileIPs.group(2);
+
+                if ("allow".equals(action)) {
+                    profileAllowedIPs
+                        .computeIfAbsent(profile, k -> new ArrayList<>())
+                        .addAll((List<String>) value);
+
+                } else if ("deny".equals(action)) {
+                    profileDeniedIPs
+                        .computeIfAbsent(profile, k -> new ArrayList<>())
+                        .addAll((List<String>) value);
+                }
+            } else {
+                switch (optionName) {
+                    default:
+                        error = true;
+                }
+            }
+        }
+
+        if (error) {
+            System.out.println("Invalid option of type " + optionValue.getClass() + ": " + optionName + " = " + optionValue);
+        }
     }
 }
--- src/main/java/com/floragunn/searchguard/sgctl/util/mapping/ir/Tls.java
+++ src/main/java/com/floragunn/searchguard/sgctl/util/mapping/ir/Tls.java
@@ -1,11 +1,8 @@
 package com.floragunn.searchguard.sgctl.util.mapping.ir;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Tls {
     boolean enabled; // activation of TLS
@@ -38,8 +35,6 @@ public class Tls {
     // IP filtering (https://www.elastic.co/docs/reference/elasticsearch/configuration-reference/security-settings#ip-filtering-settings)
     List<String> allowedIPs; // List of IP addresses to allow
     List<String> deniedIPs; // List of IP addresses to deny
-    Map<String, List<String>> profileAllowedIPs = new HashMap<>(); // transport only: List of IP addresses (value) to allow for this profile (key)
-    Map<String, List<String>> profileDeniedIPs = new HashMap<>(); // transport only: List of IP addresses (value) to deny for this profile (key)
     List<String> remoteClusterAllowedIPs; // List of IP addresses to allow for remote cluster
     List<String> remoteClusterDeniedIPs; // List of IP addresses to deny remote cluster
 
@@ -163,26 +158,8 @@ public class Tls {
                 return;
             }
 
-            // Regex for profileAllowedIPs/profileDeniedIPs with optionName profiles.$PROFILE.xpack.security.filter. allow or deny
-            Pattern profileIPsPattern = Pattern.compile("^profiles\\.([^.]+)\\.xpack\\.security\\.filter\\.(allow|deny)$");
-            Matcher mProfileIPs = profileIPsPattern.matcher(optionName);
-
             if (!(value.get(0) instanceof String)) {
                 error = true;
-            } else if(mProfileIPs.find()) {
-                String profile = mProfileIPs.group(1);
-                String action  = mProfileIPs.group(2);
-
-                if ("allow".equals(action)) {
-                    profileAllowedIPs
-                        .computeIfAbsent(profile, k -> new ArrayList<>())
-                        .addAll((List<String>) value);
-
-                } else if ("deny".equals(action)) {
-                    profileDeniedIPs
-                        .computeIfAbsent(profile, k -> new ArrayList<>())
-                        .addAll((List<String>) value);
-                }
             } else {
                 switch (optionName) {
                     case "certificate_authorities":
