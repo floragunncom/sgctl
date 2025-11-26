771ecb26ec149f8320936afdc2cd7cc8ccf1243c
Jannick Wittig <jannick.wittig@stud.uni-hannover.de>
2025-11-26 01:21:22 +0100

    added Users.java

---
+++ src/main/java/com/floragunn/searchguard/sgctl/config/xpack/Users.java
@@ -0,0 +1,73 @@
+package com.floragunn.searchguard.sgctl.config.xpack;
+
+import com.floragunn.codova.documents.DocNode;
+import com.floragunn.codova.documents.Parser;
+import com.floragunn.codova.validation.ConfigValidationException;
+import com.floragunn.codova.validation.ValidatingDocNode;
+import com.floragunn.codova.validation.ValidationErrors;
+import com.floragunn.codova.validation.errors.MissingAttribute;
+import com.floragunn.fluent.collections.ImmutableList;
+import com.floragunn.fluent.collections.ImmutableMap;
+
+
+
+public record Users(
+        ImmutableMap<String, User> mappings
+) {
+
+    //TODO: jq search query von sg blog 3 hier umsetzen, damit Nutzer nicht jq nutzen muss ?
+    //username ist der key, wird aber auch an User parse übergeben, damit User ein username attribut hat, vielleicht leichter zum migrieren
+    public static Users parse(DocNode config, Parser.Context parserContext) throws ConfigValidationException {
+        ValidatingDocNode vNode = new ValidatingDocNode(config, new ValidationErrors(), parserContext);
+
+        var builder = new ImmutableMap.Builder<String, User>(config.size());
+        for(String name: config.keySet()) {
+            //testen ob das funktioniert, ansonsten hat User kein username attribut und beim migrieren muss darauf geachtet werden (ist dann der key selbst)
+            User user = vNode.get(name).by(node -> User.parse(node, parserContext, name));
+            builder.with(name, user);
+        }
+
+        vNode.throwExceptionForPresentErrors();
+        return new Users(builder.build());
+
+    }
+
+    public record User(
+            String username,
+            String password,
+            ImmutableList<String> roles,
+            ImmutableMap<String, Object> attributes
+
+            //String email,
+            //String full_name,
+            //boolean enabled,
+            //String profile_uid
+    ) {
+
+        //TODO: brauchen vielleicht beispiele, falls ein attribut vielleicht doch nicht required ist
+        public static User parse(DocNode config, Parser.Context parserContext, String username) throws ConfigValidationException{
+            ValidationErrors vErrors = new ValidationErrors();
+            ValidatingDocNode vNode = new ValidatingDocNode(config, vErrors, parserContext);
+
+            if(!config.containsKey("username")) vErrors.add(new MissingAttribute("username", config));
+            if(!config.containsKey("password")) vErrors.add(new MissingAttribute("password", config));
+            if(!config.containsKey("roles")) vErrors.add(new MissingAttribute("roles", config));
+            if(!config.containsKey("attributes")) vErrors.add(new MissingAttribute("attributes", config));
+
+
+            User user = new User(
+                    username,
+                    vNode.get("hash").asString(),
+                    vNode.get("opendistro_security_roles").asListOfStrings(),
+                    vNode.get("attributes").asMap()
+            );
+
+            vErrors.throwExceptionForPresentErrors();
+            return user;
+
+
+        }
+
+    }
+
+}
