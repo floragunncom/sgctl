86fb17ec4b0f8edb3fa63ee998c239a937645602
Anne Rieß <anne.katharina.riess@stud.uni-hannover.de>
2025-11-26 16:59:44 +0100

    Combine SgInternalUsers.java and User.java

--- src/main/java/com/floragunn/searchguard/sgctl/config/searchguard/SgInternalUsers.java
+++ src/main/java/com/floragunn/searchguard/sgctl/config/searchguard/SgInternalUsers.java
@@ -1,6 +1,8 @@
 package com.floragunn.searchguard.sgctl.config.searchguard;
 
 import com.floragunn.fluent.collections.ImmutableList;
+import com.floragunn.fluent.collections.ImmutableMap;
+
 import java.util.LinkedHashMap;
 import java.util.Objects;
 
@@ -23,4 +25,23 @@ public record SgInternalUsers(ImmutableList<User> users) implements NamedConfig<
         }
         return result;
     }
+
+    public record User(String username, String password, ImmutableList<String> roles, ImmutableMap<String, String> metadata) {
+
+        public User {
+            Objects.requireNonNull(username, "username must not be null");
+            Objects.requireNonNull(password, "password must not be null");
+            Objects.requireNonNull(roles, "roles must not be null");
+            Objects.requireNonNull(metadata, "metadata must not be null");
+        }
+
+        public Object toBasicObject() {
+            LinkedHashMap<String, Object> result = new LinkedHashMap<>();
+            result.put("hash", password);
+            result.put("opendistro_security_roles", roles);
+            result.put("attributes", metadata);
+            return result;
+        }
+    }
+
 }
