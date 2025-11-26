d85a3f348ed25ea0d2a6cd78203a6ad9117e57f0
Thore <thore.klattenberg@stud.uni-hannover.de>
2025-11-26 16:07:05 +0100

    rename and refactor ir class so there will no conflict

---
+++ src/main/java/com/floragunn/searchguard/sgctl/util/mapping/ir/IntermediateRepresentationElasticSearchYml.java
@@ -0,0 +1,62 @@
+package com.floragunn.searchguard.sgctl.util.mapping.ir;
+
+
+public class IntermediateRepresentationElasticSearchYml {
+
+    public GlobalIR global;
+    public SslTlsIR sslTls;
+    public AuthorizationIR authoIR;
+    public AuthenticationIR authentIR;
+
+    public IntermediateRepresentationElasticSearchYml() {
+        global = new GlobalIR();
+        sslTls = new SslTlsIR();
+        authoIR = new AuthorizationIR();
+        authentIR = new AuthenticationIR();
+    }
+
+    // before setting an option, check that its type matches
+    public static boolean assertType(Object object, Class<?> type) {
+        return type.isInstance(object);
+    }
+
+    // classify into severity: 0 -> info, 1 -> needs manual rework, 2 -> critical
+    public static void errorLog(String message, int severity) {
+        switch (severity) {
+            case 0:
+                System.out.println(message);
+                break;
+            case 1:
+                System.out.println("Needs Manual rework: " + message);
+                break;
+            case 2:
+                System.out.println("Critical issue!: " + message);
+                break;
+        }
+    }
+
+}
+
+/*
+
+IR:
+global ...
+ssl/tls ...
+    - transport
+    - http
+authorization ...
+authentication ...
+    - Map<String, Object> users
+    for (String user : users)
+
+
+in searchguard:
+
+elasticsearch.yml:
+    - searchguard.nodes_dn: enumerate
+
+
+
+
+
+* */
