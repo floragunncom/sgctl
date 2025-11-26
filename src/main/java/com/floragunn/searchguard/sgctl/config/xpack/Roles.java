b5a1c3523f6edbd607f0393f405c70e9f50f4502
Julius Marquard <julius.marquard@stud.uni-hannover.de>
2025-11-26 09:16:11 +0100

    Apply formatting

--- src/main/java/com/floragunn/searchguard/sgctl/config/xpack/Roles.java
+++ src/main/java/com/floragunn/searchguard/sgctl/config/xpack/Roles.java
@@ -8,177 +8,201 @@ import com.floragunn.codova.validation.ValidationErrors;
 import com.floragunn.codova.validation.errors.MissingAttribute;
 import com.floragunn.fluent.collections.ImmutableList;
 import com.floragunn.fluent.collections.ImmutableMap;
-
 import java.util.Optional;
 
+public record Roles(ImmutableMap<String, Role> roles) {
 
-public record Roles(
-    ImmutableMap<String, Role> roles
-) {
+  public static Roles parse(DocNode doc, Parser.Context context) throws ConfigValidationException {
 
-    public static Roles parse(DocNode doc, Parser.Context context) throws ConfigValidationException {
+    var vDoc = new ValidatingDocNode(doc, new ValidationErrors(), context);
 
-        var vDoc = new ValidatingDocNode(doc, new ValidationErrors(), context);
-
-        var builder = new ImmutableMap.Builder<String, Role>(doc.size());
-        // Iterating over Roles
-        for(var name: doc.keySet()) {
-            builder.with(name, vDoc.get(name).by(Role::parse));
-        }
-
-        vDoc.throwExceptionForPresentErrors();
-
-        return new Roles(builder.build());
+    var builder = new ImmutableMap.Builder<String, Role>(doc.size());
+    // Iterating over Roles
+    for (var name : doc.keySet()) {
+      builder.with(name, vDoc.get(name).by(Role::parse));
     }
 
-    public record Role(
-            ImmutableList<String> runAs,
-            ImmutableList<String> cluster,
-            Optional<DocNode> global,
-            ImmutableList<Index> indices,
-            ImmutableList<Application> applications,
-            ImmutableList<RemoteIndex> remoteIndices,
-            ImmutableList<RemoteCluster> remoteCluster,
-            ImmutableMap<String, Object> metadata,
-            ImmutableMap<String, Object> transientMetadata,
-            Optional<String> description
-    ) {
-        public static Role parse(DocNode node, Parser.Context context) throws ConfigValidationException {
-            ValidationErrors errors = new ValidationErrors();
-            ValidatingDocNode v = new ValidatingDocNode(node, errors, context);
+    vDoc.throwExceptionForPresentErrors();
 
-            // Required. If missing, an error will be registered
-            if (!node.containsKey("cluster")) errors.add(new MissingAttribute("cluster", node));
-            if (!node.containsKey("indices")) errors.add(new MissingAttribute("indices", node));
-            if (!node.containsKey("applications")) errors.add(new MissingAttribute("applications", node));
-            if (!node.containsKey("metadata")) errors.add(new MissingAttribute("metadata", node));
+    return new Roles(builder.build());
+  }
 
-            Role role = new Role(
+  public record Role(
+      ImmutableList<String> runAs,
+      ImmutableList<String> cluster,
+      Optional<DocNode> global,
+      ImmutableList<Index> indices,
+      ImmutableList<Application> applications,
+      ImmutableList<RemoteIndex> remoteIndices,
+      ImmutableList<RemoteCluster> remoteCluster,
+      ImmutableMap<String, Object> metadata,
+      ImmutableMap<String, Object> transientMetadata,
+      Optional<String> description) {
+    public static Role parse(DocNode node, Parser.Context context)
+        throws ConfigValidationException {
+      ValidationErrors errors = new ValidationErrors();
+      ValidatingDocNode v = new ValidatingDocNode(node, errors, context);
 
-                    v.get("run_as").asList().withEmptyListAsDefault().ofStrings(),
-                    node.containsKey("cluster") ? v.get("cluster").asList().ofStrings() : ImmutableList.empty(),
-                    Optional.ofNullable(node.containsKey("global") ? v.get("global").asDocNode() : null),
+      // Required. If missing, an error will be registered
+      if (!node.containsKey("cluster")) errors.add(new MissingAttribute("cluster", node));
+      if (!node.containsKey("indices")) errors.add(new MissingAttribute("indices", node));
+      if (!node.containsKey("applications")) errors.add(new MissingAttribute("applications", node));
+      if (!node.containsKey("metadata")) errors.add(new MissingAttribute("metadata", node));
 
-                    // Parsed by other functions
-                    node.containsKey("indices") ? v.get("indices").asList().ofObjectsParsedBy(Index::parse) : ImmutableList.empty(),
-                    node.containsKey("applications") ? v.get("applications").asList().ofObjectsParsedBy(Application::parse) : ImmutableList.empty(),
-                    v.get("remote_indices").asList().withEmptyListAsDefault().ofObjectsParsedBy(RemoteIndex::parse),
-                    v.get("remote_cluster").asList().withEmptyListAsDefault().ofObjectsParsedBy(RemoteCluster::parse),
+      Role role =
+          new Role(
+              v.get("run_as").asList().withEmptyListAsDefault().ofStrings(),
+              node.containsKey("cluster")
+                  ? v.get("cluster").asList().ofStrings()
+                  : ImmutableList.empty(),
+              Optional.ofNullable(node.containsKey("global") ? v.get("global").asDocNode() : null),
 
-                    // Metadata is required
-                    node.containsKey("metadata") ? v.get("metadata").asMap() : ImmutableMap.empty(),
+              // Parsed by other functions
+              node.containsKey("indices")
+                  ? v.get("indices").asList().ofObjectsParsedBy(Index::parse)
+                  : ImmutableList.empty(),
+              node.containsKey("applications")
+                  ? v.get("applications").asList().ofObjectsParsedBy(Application::parse)
+                  : ImmutableList.empty(),
+              v.get("remote_indices")
+                  .asList()
+                  .withEmptyListAsDefault()
+                  .ofObjectsParsedBy(RemoteIndex::parse),
+              v.get("remote_cluster")
+                  .asList()
+                  .withEmptyListAsDefault()
+                  .ofObjectsParsedBy(RemoteCluster::parse),
 
-                    // Is Optional, becomes an empty map if missing
-                    node.containsKey("transient_metadata") ? v.get("transient_metadata").asMap() : ImmutableMap.empty(),
-                    Optional.ofNullable(node.containsKey("description") ? v.get("description").asString() : null)
+              // Metadata is required
+              node.containsKey("metadata") ? v.get("metadata").asMap() : ImmutableMap.empty(),
 
-            );
+              // Is Optional, becomes an empty map if missing
+              node.containsKey("transient_metadata")
+                  ? v.get("transient_metadata").asMap()
+                  : ImmutableMap.empty(),
+              Optional.ofNullable(
+                  node.containsKey("description") ? v.get("description").asString() : null));
 
-            v.throwExceptionForPresentErrors();
-            return role;
-        }
+      v.throwExceptionForPresentErrors();
+      return role;
     }
+  }
 
-    // Parser für Indices
-    public record Index(
-            ImmutableList<String> names,
-            ImmutableList<String> privileges,
-            Optional<DocNode> fieldSecurity,
-            Optional<String> query,
-            boolean allowRestrictedIndices
-    ){
-        public static Index parse(DocNode node, Parser.Context context) throws ConfigValidationException {
-            ValidationErrors errors = new ValidationErrors();
-            ValidatingDocNode v = new ValidatingDocNode(node, errors, context);
+  // Parser für Indices
+  public record Index(
+      ImmutableList<String> names,
+      ImmutableList<String> privileges,
+      Optional<DocNode> fieldSecurity,
+      Optional<String> query,
+      boolean allowRestrictedIndices) {
+    public static Index parse(DocNode node, Parser.Context context)
+        throws ConfigValidationException {
+      ValidationErrors errors = new ValidationErrors();
+      ValidatingDocNode v = new ValidatingDocNode(node, errors, context);
 
-            // Required keys
-            if (!node.containsKey("names")) errors.add(new MissingAttribute("names", node));
-            if (!node.containsKey("privileges")) errors.add(new MissingAttribute("privileges", node));
+      // Required keys
+      if (!node.containsKey("names")) errors.add(new MissingAttribute("names", node));
+      if (!node.containsKey("privileges")) errors.add(new MissingAttribute("privileges", node));
 
-            Index index = new Index(
-                node.containsKey("names") ? v.get("names").asList().ofStrings() : ImmutableList.empty(),
-                node.containsKey("privileges") ? v.get("privileges").asList().ofStrings() : ImmutableList.empty(),
-                Optional.ofNullable(node.containsKey("field_security") ? v.get("field_security").asDocNode() : null),
-                Optional.ofNullable(node.containsKey("query") ? v.get("query").asString() : null),
-                node.containsKey("allow_restricted_indices") ? v.get("allow_restricted_indices").asBoolean() : false
-            );
-            v.throwExceptionForPresentErrors();
-            return index;
-        }
+      Index index =
+          new Index(
+              node.containsKey("names")
+                  ? v.get("names").asList().ofStrings()
+                  : ImmutableList.empty(),
+              node.containsKey("privileges")
+                  ? v.get("privileges").asList().ofStrings()
+                  : ImmutableList.empty(),
+              Optional.ofNullable(
+                  node.containsKey("field_security") ? v.get("field_security").asDocNode() : null),
+              Optional.ofNullable(node.containsKey("query") ? v.get("query").asString() : null),
+              node.containsKey("allow_restricted_indices")
+                  ? v.get("allow_restricted_indices").asBoolean()
+                  : false);
+      v.throwExceptionForPresentErrors();
+      return index;
     }
+  }
 
-    // Parser for Remote Indices
-    public record RemoteIndex(
-            ImmutableList<String> clusters,
-            ImmutableList<String> names,
-            ImmutableList<String> privileges,
-            Optional<DocNode> fieldSecurity,
-            Optional<String> query,
-            boolean allowRestrictedIndices
-    ){
-        public static RemoteIndex parse(DocNode node, Parser.Context context) throws ConfigValidationException {
-            ValidationErrors errors = new ValidationErrors();
-            ValidatingDocNode v = new ValidatingDocNode(node, errors, context);
+  // Parser for Remote Indices
+  public record RemoteIndex(
+      ImmutableList<String> clusters,
+      ImmutableList<String> names,
+      ImmutableList<String> privileges,
+      Optional<DocNode> fieldSecurity,
+      Optional<String> query,
+      boolean allowRestrictedIndices) {
+    public static RemoteIndex parse(DocNode node, Parser.Context context)
+        throws ConfigValidationException {
+      ValidationErrors errors = new ValidationErrors();
+      ValidatingDocNode v = new ValidatingDocNode(node, errors, context);
 
-            // Required keys
-            if (!node.containsKey("clusters")) errors.add(new MissingAttribute("clusters", node));
-            if (!node.containsKey("names")) errors.add(new MissingAttribute("names", node));
-            if (!node.containsKey("privileges")) errors.add(new MissingAttribute("privileges", node));
+      // Required keys
+      if (!node.containsKey("clusters")) errors.add(new MissingAttribute("clusters", node));
+      if (!node.containsKey("names")) errors.add(new MissingAttribute("names", node));
+      if (!node.containsKey("privileges")) errors.add(new MissingAttribute("privileges", node));
 
-            RemoteIndex remoteindex = new RemoteIndex(
-                    node.containsKey("clusters") ? v.get("clusters").asList().ofStrings() : ImmutableList.empty(),
-                    node.containsKey("names") ? v.get("names").asList().ofStrings() : ImmutableList.empty(),
-                    node.containsKey("privileges") ? v.get("privileges").asList().ofStrings() : ImmutableList.empty(),
-                    Optional.ofNullable(node.containsKey("field_security") ? v.get("field_security").asDocNode() : null),
-                    Optional.ofNullable(node.containsKey("query") ? v.get("query").asString() : null),
-                    node.containsKey("allow_restricted_indices") ? v.get("allow_restricted_indices").asBoolean() : false
-            );
-            v.throwExceptionForPresentErrors();
-            return remoteindex;
-        }
+      RemoteIndex remoteindex =
+          new RemoteIndex(
+              node.containsKey("clusters")
+                  ? v.get("clusters").asList().ofStrings()
+                  : ImmutableList.empty(),
+              node.containsKey("names")
+                  ? v.get("names").asList().ofStrings()
+                  : ImmutableList.empty(),
+              node.containsKey("privileges")
+                  ? v.get("privileges").asList().ofStrings()
+                  : ImmutableList.empty(),
+              Optional.ofNullable(
+                  node.containsKey("field_security") ? v.get("field_security").asDocNode() : null),
+              Optional.ofNullable(node.containsKey("query") ? v.get("query").asString() : null),
+              node.containsKey("allow_restricted_indices")
+                  ? v.get("allow_restricted_indices").asBoolean()
+                  : false);
+      v.throwExceptionForPresentErrors();
+      return remoteindex;
     }
+  }
 
-    // Parser for Applications
-    public record Application(
-            String application,
-            ImmutableList<String> privileges,
-            ImmutableList<String> resources
-            ) {
-        public static Application parse(DocNode node, Parser.Context context) throws ConfigValidationException {
-            ValidationErrors errors = new ValidationErrors();
-            ValidatingDocNode v = new ValidatingDocNode(node, errors, context);
+  // Parser for Applications
+  public record Application(
+      String application, ImmutableList<String> privileges, ImmutableList<String> resources) {
+    public static Application parse(DocNode node, Parser.Context context)
+        throws ConfigValidationException {
+      ValidationErrors errors = new ValidationErrors();
+      ValidatingDocNode v = new ValidatingDocNode(node, errors, context);
 
-            // Required keys
-            if (!node.containsKey("application")) errors.add(new MissingAttribute("application", node));
-            if (!node.containsKey("privileges")) errors.add(new MissingAttribute("privileges", node));
-            if (!node.containsKey("resources")) errors.add(new MissingAttribute("resources", node));
+      // Required keys
+      if (!node.containsKey("application")) errors.add(new MissingAttribute("application", node));
+      if (!node.containsKey("privileges")) errors.add(new MissingAttribute("privileges", node));
+      if (!node.containsKey("resources")) errors.add(new MissingAttribute("resources", node));
 
-            Application application = new Application(
-                    node.containsKey("application") ? v.get("application").asString() : "",
-                    node.containsKey("privileges") ? v.get("privileges").asList().ofStrings() : ImmutableList.empty(),
-                    node.containsKey("resources") ? v.get("resources").asList().ofStrings() : ImmutableList.empty()
-            );
-            v.throwExceptionForPresentErrors();
-            return application;
-        }
+      Application application =
+          new Application(
+              node.containsKey("application") ? v.get("application").asString() : "",
+              node.containsKey("privileges")
+                  ? v.get("privileges").asList().ofStrings()
+                  : ImmutableList.empty(),
+              node.containsKey("resources")
+                  ? v.get("resources").asList().ofStrings()
+                  : ImmutableList.empty());
+      v.throwExceptionForPresentErrors();
+      return application;
     }
+  }
 
-    // Parser for Remote Clusters
-    public record RemoteCluster(
-            ImmutableList<String> clusters,
-            ImmutableList<String> privileges
-    ) {
-        public static RemoteCluster parse(DocNode node, Parser.Context context) throws ConfigValidationException {
-            ValidationErrors errors = new ValidationErrors();
-            ValidatingDocNode v = new ValidatingDocNode(node, errors, context);
+  // Parser for Remote Clusters
+  public record RemoteCluster(ImmutableList<String> clusters, ImmutableList<String> privileges) {
+    public static RemoteCluster parse(DocNode node, Parser.Context context)
+        throws ConfigValidationException {
+      ValidationErrors errors = new ValidationErrors();
+      ValidatingDocNode v = new ValidatingDocNode(node, errors, context);
 
-            RemoteCluster remoteCluster = new RemoteCluster(
-                    v.get("clusters").asList().withEmptyListAsDefault().ofStrings(),
-                    v.get("privileges").asList().withEmptyListAsDefault().ofStrings()
-            );
-            v.throwExceptionForPresentErrors();
-            return remoteCluster;
-        }
+      RemoteCluster remoteCluster =
+          new RemoteCluster(
+              v.get("clusters").asList().withEmptyListAsDefault().ofStrings(),
+              v.get("privileges").asList().withEmptyListAsDefault().ofStrings());
+      v.throwExceptionForPresentErrors();
+      return remoteCluster;
     }
-
+  }
 }
--- src/test/java/com/floragunn/searchguard/sgctl/config/xpack/RolesTest.java
+++ src/test/java/com/floragunn/searchguard/sgctl/config/xpack/RolesTest.java
@@ -1,18 +1,19 @@
 package com.floragunn.searchguard.sgctl.config.xpack;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import com.floragunn.codova.documents.DocNode;
 import com.floragunn.codova.documents.DocumentParseException;
 import com.floragunn.codova.documents.Format;
 import com.floragunn.codova.validation.ConfigValidationException;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class RolesTest {
 
-    @Test
-    void testRoleParsing() throws Exception {
-        String json = """
+  @Test
+  void testRoleParsing() throws Exception {
+    String json =
+        """
             {
               "complex_role": {
                 "run_as": ["user1"],
@@ -52,46 +53,48 @@ class RolesTest {
             }
             """;
 
-        DocNode docNode = DocNode.parse(Format.JSON).from(json);
-        Roles result = Roles.parse(docNode, null);
+    DocNode docNode = DocNode.parse(Format.JSON).from(json);
+    Roles result = Roles.parse(docNode, null);
 
-        assertNotNull(result);
-        assertTrue(result.roles().containsKey("complex_role"));
+    assertNotNull(result);
+    assertTrue(result.roles().containsKey("complex_role"));
 
-        var role = result.roles().get("complex_role");
+    var role = result.roles().get("complex_role");
 
-        // General asserts
-        assertEquals("user1", role.runAs().get(0));
-        assertEquals("monitor", role.cluster().get(0));assertTrue(role.global().isPresent());
-        assertEquals("test2", role.global().get().getAsString("test1"));
+    // General asserts
+    assertEquals("user1", role.runAs().get(0));
+    assertEquals("monitor", role.cluster().get(0));
+    assertTrue(role.global().isPresent());
+    assertEquals("test2", role.global().get().getAsString("test1"));
 
-        // Indices
-        assertEquals("admin", role.indices().get(0).names().get(0));
-        assertEquals("read", role.indices().get(0).privileges().get(0));
-        assertTrue(role.indices().get(0).allowRestrictedIndices());
+    // Indices
+    assertEquals("admin", role.indices().get(0).names().get(0));
+    assertEquals("read", role.indices().get(0).privileges().get(0));
+    assertTrue(role.indices().get(0).allowRestrictedIndices());
 
-        // Applications
-        assertEquals("kibana", role.applications().get(0).application());
-        assertEquals("read", role.applications().get(0).privileges().get(0));
-        assertEquals("*", role.applications().get(0).resources().get(0));
+    // Applications
+    assertEquals("kibana", role.applications().get(0).application());
+    assertEquals("read", role.applications().get(0).privileges().get(0));
+    assertEquals("*", role.applications().get(0).resources().get(0));
 
-        // Remote indices
-        assertEquals("test-cluster1", role.remoteIndices().get(0).clusters().get(0));
-        assertEquals("boss2", role.remoteIndices().get(0).names().get(0));
-        assertEquals("read", role.remoteIndices().get(0).privileges().get(0));
+    // Remote indices
+    assertEquals("test-cluster1", role.remoteIndices().get(0).clusters().get(0));
+    assertEquals("boss2", role.remoteIndices().get(0).names().get(0));
+    assertEquals("read", role.remoteIndices().get(0).privileges().get(0));
 
-        // Remote cluster
-        assertEquals("test-cluster2", role.remoteCluster().get(0).clusters().get(0));
-        assertEquals("all", role.remoteCluster().get(0).privileges().get(0));
+    // Remote cluster
+    assertEquals("test-cluster2", role.remoteCluster().get(0).clusters().get(0));
+    assertEquals("all", role.remoteCluster().get(0).privileges().get(0));
 
-        // Rest
-        assertTrue(role.description().isPresent());
-        assertEquals("Beautiful description", role.description().get());
-    }
+    // Rest
+    assertTrue(role.description().isPresent());
+    assertEquals("Beautiful description", role.description().get());
+  }
 
-    @Test
-    void testSmallRoleParsing() throws Exception {
-        String json = """
+  @Test
+  void testSmallRoleParsing() throws Exception {
+    String json =
+        """
             {
               "minimal_role": {
                 "cluster": ["all"],
@@ -102,31 +105,32 @@ class RolesTest {
             }
             """;
 
-        DocNode docNode = DocNode.parse(Format.JSON).from(json);
-        Roles result = Roles.parse(docNode, null);
+    DocNode docNode = DocNode.parse(Format.JSON).from(json);
+    Roles result = Roles.parse(docNode, null);
 
-        var role = result.roles().get("minimal_role");
+    var role = result.roles().get("minimal_role");
 
-        assertEquals("all", role.cluster().get(0));
-        assertTrue(role.indices().isEmpty());
-        assertTrue(role.applications().isEmpty());
-        assertTrue(role.runAs().isEmpty());
-        assertTrue(role.description().isEmpty());
-    }
+    assertEquals("all", role.cluster().get(0));
+    assertTrue(role.indices().isEmpty());
+    assertTrue(role.applications().isEmpty());
+    assertTrue(role.runAs().isEmpty());
+    assertTrue(role.description().isEmpty());
+  }
 
-    @Test
-    void testEmptyDocumentParsing() throws Exception {
-        DocNode docNode = DocNode.parse(Format.JSON).from("{}");
-        Roles result = Roles.parse(docNode, null);
+  @Test
+  void testEmptyDocumentParsing() throws Exception {
+    DocNode docNode = DocNode.parse(Format.JSON).from("{}");
+    Roles result = Roles.parse(docNode, null);
 
-        assertNotNull(result);
-        assertTrue(result.roles().isEmpty());
-    }
+    assertNotNull(result);
+    assertTrue(result.roles().isEmpty());
+  }
 
-    @Test
-    void testMissingRequiredFields() throws DocumentParseException {
-        // indices and applications (required) missing
-        String json = """
+  @Test
+  void testMissingRequiredFields() throws DocumentParseException {
+    // indices and applications (required) missing
+    String json =
+        """
             {
               "broken_role": {
                 "cluster": ["all"],
@@ -135,17 +139,20 @@ class RolesTest {
             }
             """;
 
-        DocNode docNode = DocNode.parse(Format.JSON).from(json);
+    DocNode docNode = DocNode.parse(Format.JSON).from(json);
 
-        assertThrows(ConfigValidationException.class, () -> {
-            Roles.parse(docNode, null);
+    assertThrows(
+        ConfigValidationException.class,
+        () -> {
+          Roles.parse(docNode, null);
         });
-    }
+  }
 
-    @Test
-    void testMissingNestedRequiredField() throws DocumentParseException {
-        // applications.privileges (required) missing
-        String json = """
+  @Test
+  void testMissingNestedRequiredField() throws DocumentParseException {
+    // applications.privileges (required) missing
+    String json =
+        """
             {
               "broken_nested_role": {
                 "cluster": [], "indices": [], "metadata": {},
@@ -159,10 +166,12 @@ class RolesTest {
             }
             """;
 
-        DocNode docNode = DocNode.parse(Format.JSON).from(json);
+    DocNode docNode = DocNode.parse(Format.JSON).from(json);
 
-        assertThrows(ConfigValidationException.class, () -> {
-            Roles.parse(docNode, null);
+    assertThrows(
+        ConfigValidationException.class,
+        () -> {
+          Roles.parse(docNode, null);
         });
-    }
+  }
 }
