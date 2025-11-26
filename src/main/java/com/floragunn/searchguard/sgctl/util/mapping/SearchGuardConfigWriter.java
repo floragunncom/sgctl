082af06902ccf52640b3b3c7e3b82a8a735200c5
Eduard Schleicher <eduard.schleicher@stud.uni-hannover.de>
2025-11-26 18:05:40 +0100

    Implements the foundational structure for the SearchGuardConfigWriter class to begin the output configuration generation.

--- src/main/java/com/floragunn/searchguard/sgctl/util/mapping/SearchGuardConfigWriter.java
+++ src/main/java/com/floragunn/searchguard/sgctl/util/mapping/SearchGuardConfigWriter.java
@@ -1,4 +1,55 @@
 package com.floragunn.searchguard.sgctl.util.mapping;
 
+import com.floragunn.searchguard.sgctl.commands.MigrateSecurity;
+import com.floragunn.searchguard.sgctl.util.mapping.ir.elasticSearchYml.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+
+/**
+ * Top-level configuration object for sg_authc.yml.
+ */
 public class SearchGuardConfigWriter {
+
+    public SearchGuardConfigWriter() {
+        try {
+            generateAllConfigs();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Coordinates the generation and writing of all Search Guard configuration files.
+     * @throws IOException If an error occurs during file writing.
+     */
+    public void generateAllConfigs() throws IOException {
+       IntermediateRepresentationElasticSearchYml IntermediateRepresentationElasticSearchYml = new IntermediateRepresentationElasticSearchYml();
+
+        Object authcConfigObject = createAuthcConfig(IntermediateRepresentationElasticSearchYml);
+
+        writeYamlConfig(authcConfigObject, MigrateSecurity.outputDir, "sg_authc.yml");
+    }
+
+    /**
+     * Implements the core logic for mapping X-Pack Authentication realms (LDAP, SAML, OIDC)
+     * to the Search Guard sg_authc.yml configuration structure.
+     * @param ir The Intermediate Representation.
+     * @return The fully populated SearchGuardAuthcConfig object.
+     */
+    private Object createAuthcConfig(IntermediateRepresentationElasticSearchYml ir) {
+        return Collections.emptyMap();
+    }
+
+    /**
+     * Serializes a Java object (the target configuration model) into a YAML file using Jackson.
+     * @param configObject The Java object to serialize (e.g., SearchGuardAuthcConfig).
+     * @param outputDir The target directory (MS8).
+     * @param filename The output filename (e.g., "sg_authc.yml").
+     * @throws IOException If an I/O error occurs.
+     */
+    private void writeYamlConfig(Object configObject, File outputDir, String filename) {
+    }
+
 }
