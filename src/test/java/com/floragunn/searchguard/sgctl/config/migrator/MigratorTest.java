62c7b90071771a7b58a86a5bbf7160149ead0331
Justus Laube <1717-jlaube@users.noreply.git.se.uni-hannover.de>
2025-11-29 13:15:19 +0100

    MigratorTest: Completed first simple test

--- src/test/java/com/floragunn/searchguard/sgctl/config/migrator/MigratorTest.java
+++ src/test/java/com/floragunn/searchguard/sgctl/config/migrator/MigratorTest.java
@@ -1,39 +1,99 @@
 package com.floragunn.searchguard.sgctl.config.migrator;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import com.floragunn.searchguard.sgctl.config.migrate.Migrator;
 import com.floragunn.searchguard.sgctl.config.migrate.MigratorRegistry;
 import com.floragunn.searchguard.sgctl.config.migrate.SubMigrator;
 import com.floragunn.searchguard.sgctl.config.searchguard.NamedConfig;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class MigratorTest {
-  @Test
-  public void testMigration() {
-    // TODO: Finish Test
 
-    SubMigrator subMigrator =
-        new SubMigrator() {
-          @Override
-          public List<NamedConfig<?>> migrate(Migrator.MigrationContext context, Logger logger) {
-            return List.of();
-          }
-        };
-    MigratorRegistry.registerSubMigratorStatic(subMigrator);
-    MigratorRegistry.finalizeMigratorsStatic();
+  static class TestMigratorUsers implements SubMigrator {
 
-    Migrator migrator = new Migrator();
+    public static class SgInteralUsersConfig implements NamedConfig<SgInteralUsersConfig> {
+      @Override
+      public Object toBasicObject() {
+        return Map.of("users", List.of("admin", "cthon98", "AzureDiamond"));
+      }
 
-    Migrator.MigrationContext context = new Migrator.MigrationContext(null, null);
+      @Override
+      public String getFileName() {
+        return "sg_interal_users.yml";
+      }
+    }
 
-    migrator.migrate(context);
+    @Override
+    public List<NamedConfig<?>> migrate(Migrator.MigrationContext context, Logger logger) {
+      logger.debug("MigratorA migrate start");
 
-    assertTrue(true);
+      final SgInteralUsersConfig sgInternalUsersConfig = new SgInteralUsersConfig();
+      final List<NamedConfig<?>> namedConfigs = List.of(sgInternalUsersConfig);
+      return namedConfigs;
+    }
   }
 
-  private static final Logger logger = LoggerFactory.getLogger(MigratorTest.class);
+  @Test
+  public void testMigrationSimple() {
+    // Register all sub-migrators
+    MigratorRegistry.registerSubMigratorStatic(new TestMigratorUsers());
+    // finalize to prevent error
+    MigratorRegistry.finalizeMigratorsStatic();
+
+    final Migrator migrator = new Migrator();
+
+    // Do the migration
+    Migrator.MigrationContext context = new Migrator.MigrationContext(null, null);
+    final List<NamedConfig<?>> migrationResult;
+    try {
+      migrationResult = migrator.migrate(context);
+    } catch (IllegalStateException e) {
+      System.err.println("MigratorA migrate failed. Did you forget to finalize?");
+      throw e;
+    }
+
+    // ==== Testing the outputed migration ====
+
+    // Migration should have worked
+    assertNotNull(migrationResult);
+    // We expect one sg_interal_users.yml output file
+    assertEquals(1, migrationResult.size());
+
+    // Check coherence of output config
+    final var config = migrationResult.get(0);
+    assertEquals("sg_interal_users.yml", config.getFileName());
+    // Also check for exact class, because theoretically a different class could also provide
+    // sg_interal_users.yml
+    if (config.getClass() == TestMigratorUsers.SgInteralUsersConfig.class) {
+      // all ok
+    } else {
+      throw new IllegalStateException(
+          String.format(
+              "Output config has wrong class (%s), should be %s",
+              config.getClass().getName(), TestMigratorUsers.class.getName()));
+    }
+    // Check innards
+    final TestMigratorUsers.SgInteralUsersConfig testInteralUsersConfig =
+        (TestMigratorUsers.SgInteralUsersConfig) config;
+    final var basicObject = testInteralUsersConfig.toBasicObject();
+    assertInstanceOf(Map.class, basicObject);
+    final var configMap = (Map<?, ?>) basicObject;
+    assertEquals(1, configMap.size());
+    final var usersObject = configMap.get("users");
+    assertNotNull(usersObject);
+    assertInstanceOf(List.class, usersObject);
+    final var users = (List<?>) usersObject;
+    final var expectedUsers = List.of("admin", "cthon98", "AzureDiamond");
+    assertEquals(expectedUsers.size(), users.size());
+    for (int i = 0; i < expectedUsers.size(); i++) {
+      final var expectedUser = expectedUsers.get(i);
+      final var actualUser = users.get(i);
+      assertInstanceOf(String.class, actualUser);
+      assertEquals(expectedUser, actualUser);
+    }
+  }
 }
