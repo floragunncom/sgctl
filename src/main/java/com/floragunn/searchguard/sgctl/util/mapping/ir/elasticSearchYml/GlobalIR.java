49a18b303dc828457c4ef8cc7ff82ae851b49919
Thore <thore.klattenberg@stud.uni-hannover.de>
2025-11-30 21:41:46 +0100

    added global options

--- src/main/java/com/floragunn/searchguard/sgctl/util/mapping/ir/elasticSearchYml/GlobalIR.java
+++ src/main/java/com/floragunn/searchguard/sgctl/util/mapping/ir/elasticSearchYml/GlobalIR.java
@@ -4,4 +4,17 @@ public class GlobalIR {
 
     boolean xpackSecEnabled;
 
+    public void handleGlobalOptions(String optionName, Object optionValue) {
+        boolean error = false;
+
+        // Booleans
+        if (IntermediateRepresentationElasticSearchYml.assertType(optionValue, Boolean.class)) {
+            boolean value = (Boolean) optionValue;
+            switch (optionName) {
+                case "enabled":
+                    xpackSecEnabled = value;
+            }
+        }
+
+    }
 }
