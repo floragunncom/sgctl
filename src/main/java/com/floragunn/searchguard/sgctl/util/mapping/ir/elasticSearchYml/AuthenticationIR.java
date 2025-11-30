73ea1e4cc4fa156118c2a46951f3f618ff4998f2
Thore <thore.klattenberg@stud.uni-hannover.de>
2025-11-30 22:59:01 +0100

    something like this should be able to deal with the different realms, need to implement them next

--- src/main/java/com/floragunn/searchguard/sgctl/util/mapping/ir/elasticSearchYml/AuthenticationIR.java
+++ src/main/java/com/floragunn/searchguard/sgctl/util/mapping/ir/elasticSearchYml/AuthenticationIR.java
@@ -2,6 +2,7 @@ package com.floragunn.searchguard.sgctl.util.mapping.ir.elasticSearchYml;
 
 import com.floragunn.searchguard.sgctl.util.mapping.ir.IntermediateRepresentation;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,7 +20,21 @@ public class AuthenticationIR {
 
         // realms, they have this pattern: xpack.security.authc.realms.<type>.<name>.<setting>
         if (optionName.startsWith("realms.")) {
+            String substring = optionName.substring("realms.".length());
+            String[] parts = substring.split("\\.");
 
+            if (parts.length < 3) {
+                System.out.println("Invalid option: %s" + substring);
+                return;
+            }
+
+            String type = parts[0]; // ldap, ...
+            String name = parts[1]; // ldap1, ...
+            String attr = String.join(".", Arrays.copyOfRange(parts, 2, parts.length));
+
+            RealmIR realm = realms.computeIfAbsent(name, n -> RealmIR.create(type, n));
+
+            realm.handleAttribute(attr, optionValue);
         }
 
         // Booleans
--- src/main/java/com/floragunn/searchguard/sgctl/util/mapping/ir/elasticSearchYml/RealmIR.java
+++ src/main/java/com/floragunn/searchguard/sgctl/util/mapping/ir/elasticSearchYml/RealmIR.java
@@ -3,7 +3,32 @@ package com.floragunn.searchguard.sgctl.util.mapping.ir.elasticSearchYml;
 import java.util.HashMap;
 import java.util.Map;
 
-public class RealmIR {
+ public abstract class RealmIR {
     String type; // ldap, saml, oidc, ...
     String name;
+    int order;
+
+    public RealmIR(String type, String name) {
+        this.type = type;
+        this.name = name;
+    }
+
+    // each realm type implements its own handler, attribute is suffix after xpack.security.authc.realms.<type>.<name>.
+    public abstract void handleAttribute(String attribute, Object value);
+
+    public static RealmIR create(String type, String name) {
+        switch(type) {
+            case "ldap": return new LdapRealmIR(type, name);
+            case "file": return new FileRealmIR(type, name);
+            case "native": return new NativeRealmIR(type, name);
+            case "saml": return new SamlRealmIR(type, name);
+            case "pki": return new PkiRealmIR(type, name);
+            case "oidc": return new OidcRealmIR(type, name);
+            case "kerberos": return new KerberosRealmIR(type, name);
+            default:
+                System.out.println("Unknown realm type: " + type);
+                return new UnknownRealmIR(type, name);
+        }
+    }
+
 }
