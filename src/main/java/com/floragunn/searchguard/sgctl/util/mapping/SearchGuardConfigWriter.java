cec6fe57d33d580043734f498b44c8e97ab723f1
Etienne <ben.etienne.altmann@stud.uni-hannover.de>
2025-12-01 12:33:42 +0100

    Added Realm IR Support and changed location for better clarity

--- src/main/java/com/floragunn/searchguard/sgctl/util/mapping/SearchGuardConfigWriter.java
+++ src/main/java/com/floragunn/searchguard/sgctl/util/mapping/SearchGuardConfigWriter.java
@@ -1,14 +1,13 @@
 package com.floragunn.searchguard.sgctl.util.mapping;
 
 import com.floragunn.codova.documents.DocWriter;
-import com.floragunn.searchguard.sgctl.commands.MigrateConfig.SgAuthc;
-import com.floragunn.searchguard.sgctl.commands.MigrateConfig.NewAuthDomain;
-import com.floragunn.searchguard.sgctl.util.mapping.ir.elasticSearchYml.*;
+import com.floragunn.searchguard.sgctl.commands.MigrateConfig;
+import com.floragunn.searchguard.sgctl.util.mapping.ir.elasticSearchYml.IntermediateRepresentationElasticSearchYml;
+import com.floragunn.searchguard.sgctl.util.mapping.writer.SGAuthcTranslator;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.*;
 
 /**
  * Top-level configuration writer for Search Guard.
@@ -27,67 +26,9 @@ public class SearchGuardConfigWriter {
     public static void generateAllConfigs(File outputDir) throws IOException {
         IntermediateRepresentationElasticSearchYml ir = new IntermediateRepresentationElasticSearchYml();
 
-        SgAuthc authcConfig = createAuthcConfig(ir);
+        MigrateConfig.SgAuthc authcConfig = SGAuthcTranslator.createAuthcConfig(ir);
 
         writeYamlConfig(authcConfig, outputDir, "sg_authc.yml");
-    }
-
-    /**
-     * Maps LDAP configuration from intermediate representation to Search Guard's sg_authc.yml format.
-     *
-     * @param ir The intermediate representation.
-     * @return Populated SgAuthc object.
-     */
-    private static SgAuthc createAuthcConfig(IntermediateRepresentationElasticSearchYml ir) {
-        SgAuthc config = new SgAuthc();
-        config.authDomains = new ArrayList<>();
-        config.internalProxies = "";
-        config.remoteIpHeader = "";
-
-        config.authDomains.add(createLdapDomain(ir));
-
-        config.authDomains.add(createOidcDomain(ir));
-
-        return config;
-    }
-
-    /**
-     * Creates the LDAP-Auth-Domain for sg_authc.yml
-     * @param ir The IR that holds the config info
-     * @return NewAuthDomain
-     */
-    private static NewAuthDomain createLdapDomain(IntermediateRepresentationElasticSearchYml ir) {
-        Map<String, Object> ldapConfig = new HashMap<>();
-        List<String> ldapHosts = Arrays.asList("ldap.example.com", "other.example.com");
-        ldapConfig.put("ldap.idp.hosts", ldapHosts);
-
-        return new NewAuthDomain(
-                "basic/ldap",
-                null,
-                null,
-                null,
-                ldapConfig,
-                null
-        );
-    }
-
-    /**
-     * Creates the OIDC-Auth-Domain for sg_authc.yml
-     * @param ir The IR that holds the config info
-     * @return NewAuthDomain
-     */
-    private static NewAuthDomain createOidcDomain(IntermediateRepresentationElasticSearchYml ir) {
-        Map<String, Object> oidcConfig = new HashMap<>();
-
-
-        return new NewAuthDomain(
-                "oidc",
-                null,
-                null,
-                null,
-                oidcConfig,
-                null
-        );
     }
 
 
