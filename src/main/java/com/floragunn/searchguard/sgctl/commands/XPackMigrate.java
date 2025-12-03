bc09b09d99dbfde425b8a10aaa08f6ee1c3bfe15
Jannick Wittig <jannick.wittig@stud.uni-hannover.de>
2025-12-03 14:12:33 +0100

    added User SubMigrator to XPackMigrate

--- src/main/java/com/floragunn/searchguard/sgctl/commands/XPackMigrate.java
+++ src/main/java/com/floragunn/searchguard/sgctl/commands/XPackMigrate.java
@@ -8,9 +8,11 @@ import com.floragunn.codova.validation.ConfigValidationException;
 import com.floragunn.searchguard.sgctl.SgctlException;
 import com.floragunn.searchguard.sgctl.config.migrate.Migrator;
 import com.floragunn.searchguard.sgctl.config.migrate.MigratorRegistry;
+import com.floragunn.searchguard.sgctl.config.migrate.UserMigrator;
 import com.floragunn.searchguard.sgctl.config.searchguard.NamedConfig;
 import com.floragunn.searchguard.sgctl.config.xpack.RoleMappings;
 import com.floragunn.searchguard.sgctl.config.xpack.Roles;
+import com.floragunn.searchguard.sgctl.config.xpack.Users;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -47,8 +49,12 @@ public class XPackMigrate implements Callable<Integer> {
   private static final Map<String, Parser<Object, Parser.Context>> configParsers =
       Map.of(
           // TODO: Add parsing functions here <filename>,Record::parse
-          "role_mapping.json", RoleMappings::parse,
-          "roles.json", Roles::parse);
+          "role_mapping.json",
+          RoleMappings::parse,
+          "roles.json",
+          Roles::parse,
+          "users.json",
+          Users::parse);
 
   public Integer call() throws Exception {
     registerSubMigrators();
@@ -79,7 +85,7 @@ public class XPackMigrate implements Callable<Integer> {
     return new Migrator.MigrationContext(
         Optional.ofNullable((RoleMappings) xPackConfigs.get("role_mappings.json")),
         Optional.ofNullable((Roles) xPackConfigs.get("roles.json")),
-        Optional.empty(), // TODO: Get real config
+        Optional.ofNullable((Users) xPackConfigs.get("users.json")),
         Optional.empty(), // TODO: Get real config
         Optional.empty() // TODO: Get real config
         );
@@ -88,7 +94,7 @@ public class XPackMigrate implements Callable<Integer> {
   private void registerSubMigrators() {
     // TODO: Add sub migrators example:
     // MigratorRegistry.registerSubMigratorStatic(...);
-
+    MigratorRegistry.registerSubMigratorStatic(new UserMigrator());
     MigratorRegistry.finalizeMigratorsStatic(); // Never forget
   }
 
