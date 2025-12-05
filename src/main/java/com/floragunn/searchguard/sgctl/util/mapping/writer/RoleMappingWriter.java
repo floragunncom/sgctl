62666d48103e4d27a1f7c127e00c52f1aea67705
ramon.tebbe <ramon.tebbe@stud.uni-hannover.de>
2025-12-05 16:46:25 +0100

    Added more simple structure to RoleMappingWriter

--- src/main/java/com/floragunn/searchguard/sgctl/util/mapping/writer/RoleMappingWriter.java
+++ src/main/java/com/floragunn/searchguard/sgctl/util/mapping/writer/RoleMappingWriter.java
@@ -1,28 +1,38 @@
 package com.floragunn.searchguard.sgctl.util.mapping.writer;
 
+import com.floragunn.codova.documents.DocWriter;
 import com.floragunn.codova.documents.Document;
 import com.floragunn.searchguard.sgctl.util.mapping.MigrationReport;
 import com.floragunn.searchguard.sgctl.util.mapping.ir.IntermediateRepresentation;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
 
 public class RoleMappingWriter implements Document<RoleMappingWriter>{
     private IntermediateRepresentation ir;
     private MigrationReport report;
     private List<SGRoleMapping> roles;
 
+    private static final String FILE_NAME = "sg_roles_mapping.yml";
+
     public RoleMappingWriter(IntermediateRepresentation ir) {
         this.ir = ir;
         this.report = MigrationReport.shared;
+        createSGRoleMappings();
+        print(DocWriter.yaml().writeAsString(this));
     }
 
     public void createSGRoleMappings() {
-
+        //TODO: implement
     }
 
     @Override
     public Object toBasicObject() {
-        return null;
+        Map<String, SGRoleMapping> contents = new LinkedHashMap<>();
+        //TODO: implement
+        return contents;
     }
 
     static class SGRoleMapping implements Document<SGRoleMapping> {
@@ -31,5 +41,13 @@ public class RoleMappingWriter implements Document<RoleMappingWriter>{
         public Object toBasicObject() {
             return null;
         }
+    }
+
+    static void print(Object line) {
+        System.out.println(line);
+    }
+
+    static void printErr(Object line) {
+        System.err.println(line);
     }
 }
