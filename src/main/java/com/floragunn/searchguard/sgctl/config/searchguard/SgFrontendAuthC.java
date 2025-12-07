e7c210be1d5293fc95ca04bb61127bc8b2e83eca
Johannes-Hauke Heiß <johannes-hauke.heiss@stud.uni-hannover.de>
2025-12-07 23:12:00 +0100

    Initial implementation of SgFrontendAuthC

---
+++ src/main/java/com/floragunn/searchguard/sgctl/config/searchguard/SgFrontendAuthC.java
@@ -0,0 +1,30 @@
+package com.floragunn.searchguard.sgctl.config.searchguard;
+
+import com.floragunn.codova.documents.Document;
+import com.floragunn.fluent.collections.ImmutableList;
+import com.floragunn.fluent.collections.OrderedImmutableMap;
+import java.util.Objects;
+
+public record SgFrontendAuthC(ImmutableList<AuthDomain<?>> authDomains)
+    implements NamedConfig<SgFrontendAuthC> {
+
+  public SgFrontendAuthC {
+    Objects.requireNonNull(authDomains, "authDomains must not be null");
+  }
+
+  @Override
+  public String getFileName() {
+    return "sg_frontend_authc.yml";
+  }
+
+  @Override
+  public Object toBasicObject() {
+    var builder = new OrderedImmutableMap.Builder<String, Object>();
+    var authDomainsBuilder = new OrderedImmutableMap.Builder<String, Object>();
+    authDomainsBuilder.put("auth_domains", authDomains);
+    builder.put("default", authDomainsBuilder.build());
+    return builder.build();
+  }
+
+  public interface AuthDomain<T> extends Document<T> {}
+}
