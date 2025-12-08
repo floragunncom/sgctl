7e47f33952d0c78a8fc5a03f03de1099975c06b8
Ben Fischer <ben.josef.fischer@stud.uni-hannover.de>
2025-12-09 00:10:19 +0100

    test: expand elasticsearch yaml reader coverage

--- src/test/java/com/floragunn/searchguard/sgctl/util/mapping/ir/ElasticsearchYamlReaderTest.java
+++ src/test/java/com/floragunn/searchguard/sgctl/util/mapping/ir/ElasticsearchYamlReaderTest.java
@@ -49,21 +49,32 @@ class ElasticsearchYamlReaderTest extends TestBase {
         assertTrue(http.getEnabled());
         assertEquals("jks", http.getKeystoreType());
         assertEquals("certs/http.p12", http.getKeystorePath());
+        assertEquals("httpStorePass", http.getKeystorePassword());
+        assertEquals("httpKeyPass", http.getKeystoreKeyPassword());
+        assertEquals("certs/http-trust.p12", http.getTruststorePath());
+        assertEquals("httpTrustPass", http.getTruststorePassword());
+        assertEquals("PKCS12", http.getTruststoreType());
         assertEquals("http.crt", http.getCertificatePath());
         assertEquals("http.key", http.getPrivateKeyPath());
         assertEquals("httpSecret", http.getPrivateKeyPassword());
-        assertEquals(List.of("http-ca.pem"), http.getCertificateAuthorities());
-        assertEquals(List.of("TLSv1.2"), http.getSupportedProtocols());
-        assertEquals(List.of("TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"), http.getCiphers());
+        assertEquals("full", http.getVerificationMode());
+        assertEquals("none", http.getClientAuthMode());
+        assertEquals(List.of("http-ca.pem", "http-ca2.pem"), http.getCertificateAuthorities());
+        assertEquals(List.of("TLSv1.3", "TLSv1.2"), http.getSupportedProtocols());
+        assertEquals(List.of("TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256", "TLS_AES_256_GCM_SHA384"), http.getCiphers());
         assertEquals(List.of("127.0.0.1"), http.getAllowedIPs());
+        assertEquals(List.of("10.10.0.0/16"), http.getDeniedIPs());
 
         var transport = ir.getSslTls().getTransport();
         assertTrue(transport.getEnabled());
         assertEquals("certificate", transport.getVerificationMode());
         assertEquals("optional", transport.getClientAuthMode());
         assertEquals("certs/transport.p12", transport.getKeystorePath());
+        assertEquals("changeit", transport.getKeystorePassword());
+        assertEquals("transKeyPass", transport.getKeystoreKeyPassword());
         assertEquals("PKCS12", transport.getKeystoreType());
         assertEquals("certs/transport-trust.p12", transport.getTruststorePath());
+        assertEquals("transTrustPass", transport.getTruststorePassword());
         assertEquals("PKCS12", transport.getTruststoreType());
         assertEquals(List.of("ca1.pem", "ca2.pem"), transport.getCertificateAuthorities());
         assertEquals(List.of("TLSv1.3", "TLSv1.2"), transport.getSupportedProtocols());
@@ -75,24 +86,27 @@ class ElasticsearchYamlReaderTest extends TestBase {
 
         assertEquals(List.of("192.168.1.10"), ir.getSslTls().getProfileAllowedIPs().get("edge"));
         assertEquals(List.of("192.168.1.0/24"), ir.getSslTls().getProfileDeniedIPs().get("edge"));
+        assertEquals(List.of("10.0.0.0/8"), ir.getSslTls().getProfileAllowedIPs().get("backend"));
+        assertEquals(List.of("172.16.0.0/12"), ir.getSslTls().getProfileDeniedIPs().get("backend"));
 
         var auth = ir.getAuthent();
         assertTrue(auth.getTokenEnabled());
-        assertEquals("20m", auth.getTokenTimeout());
+        assertEquals("30m", auth.getTokenTimeout());
         assertEquals("anon_user", auth.getAnonymousUserName());
         assertEquals("sg_anonymous", auth.getAnonymousRoles());
         assertTrue(auth.getAnonymousAuthzException());
         assertTrue(auth.getApiKeyEnabled());
-        assertEquals("30m", auth.getApiKeyCacheTtl());
-        assertEquals("5000", auth.getMaxTokens());
+        assertEquals("45m", auth.getApiKeyCacheTtl());
+        assertEquals("8000", auth.getMaxTokens());
         assertEquals("pbkdf2", auth.getApiKeyInMemoryHashingAlgorithm());
-        assertEquals("7d", auth.getApiKeyRetentionPeriod());
-        assertEquals("1h", auth.getApiKeyDeleteInterval());
-        assertEquals("30s", auth.getApiKeyDeleteTimeout());
+        assertEquals("14d", auth.getApiKeyRetentionPeriod());
+        assertEquals("2h", auth.getApiKeyDeleteInterval());
+        assertEquals("1m", auth.getApiKeyDeleteTimeout());
         assertEquals("pbkdf2_1000", auth.getApiKeyHashingAlgorithm());
+        assertEquals("pbkdf2_1000", auth.getPasswordHashingAlgoritm());
 
         var realms = auth.getRealms();
-        assertEquals(4, realms.size());
+        assertEquals(7, realms.size());
 
         var ldap = (RealmIR.LdapRealmIR) realms.get("corp");
         assertNotNull(ldap);
@@ -112,6 +126,14 @@ class ElasticsearchYamlReaderTest extends TestBase {
         assertTrue(fileRealm.isEnabled());
         assertEquals(1, fileRealm.getOrder());
 
+        var nativeRealm = (RealmIR.NativeRealmIR) realms.get("native1");
+        assertNotNull(nativeRealm);
+        assertEquals("native", nativeRealm.getType());
+        assertEquals("5m", nativeRealm.getCacheTtl());
+        assertEquals(500, nativeRealm.getCacheMaxUsers());
+        assertTrue(nativeRealm.isEnabled());
+        assertEquals(2, nativeRealm.getOrder());
+
         var pkiRealm = (RealmIR.PkiRealmIR) realms.get("pki1");
         assertNotNull(pkiRealm);
         assertEquals(List.of("pki-ca.pem"), pkiRealm.getCertificateAuthorities());
@@ -120,8 +142,9 @@ class ElasticsearchYamlReaderTest extends TestBase {
         assertEquals("certs/pki-trust.p12", pkiRealm.getTruststorePath());
         assertEquals("PKCS12", pkiRealm.getTruststoreType());
         assertEquals("changeit", pkiRealm.getTruststorePassword());
+        assertEquals(Boolean.TRUE, pkiRealm.getDelegationEnabled());
         assertTrue(pkiRealm.isEnabled());
-        assertEquals(2, pkiRealm.getOrder());
+        assertEquals(4, pkiRealm.getOrder());
 
         var oidcRealm = (RealmIR.OidcRealmIR) realms.get("oidc1");
         assertNotNull(oidcRealm);
@@ -137,7 +160,27 @@ class ElasticsearchYamlReaderTest extends TestBase {
         assertEquals("email", oidcRealm.getClaimMail());
         assertEquals("groups", oidcRealm.getClaimGroups());
         assertTrue(oidcRealm.isEnabled());
-        assertEquals(3, oidcRealm.getOrder());
+        assertEquals(5, oidcRealm.getOrder());
+
+        var samlRealm = (RealmIR.SamlRealmIR) realms.get("saml1");
+        assertNotNull(samlRealm);
+        assertEquals("saml", samlRealm.getType());
+        assertEquals("/etc/idp.xml", samlRealm.getIdpMetadataPath());
+        assertEquals("sg-sp", samlRealm.getSpEntityID());
+        assertEquals("https://sg.example.com/acs", samlRealm.getSpAcs());
+        assertEquals("username", samlRealm.getAttributesPrincipal());
+        assertTrue(samlRealm.isEnabled());
+        assertEquals(3, samlRealm.getOrder());
+
+        var kerberosRealm = (RealmIR.KerberosRealmIR) realms.get("krb1");
+        assertNotNull(kerberosRealm);
+        assertEquals("kerberos", kerberosRealm.getType());
+        assertEquals("/etc/keytab", kerberosRealm.getKeytabPath());
+        assertEquals("HTTP/host@REALM", kerberosRealm.getPrincipal());
+        assertEquals(Boolean.TRUE, kerberosRealm.getKrbDebug());
+        assertEquals(Boolean.FALSE, kerberosRealm.getRemoveRealmName());
+        assertTrue(kerberosRealm.isEnabled());
+        assertEquals(6, kerberosRealm.getOrder());
     }
 
     private IntermediateRepresentationElasticSearchYml readIr(String resourceName) {
