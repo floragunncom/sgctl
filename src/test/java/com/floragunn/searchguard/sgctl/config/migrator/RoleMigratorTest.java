5b9f1702fc9a50f392f19cb4c90013fe09ff6b52
THarries <timonharries@googlemail.com>
2025-12-09 12:07:13 +0100

    Initialize branch for Role Migrator Tests with todos

---
+++ src/test/java/com/floragunn/searchguard/sgctl/config/migrator/RoleMigratorTest.java
@@ -0,0 +1,13 @@
+package com.floragunn.searchguard.sgctl.config.migrator;
+
+public class RoleMigratorTest {
+    //todo check correct replacement
+    //todo check removal of unmigratable privileges
+    //todo index & cluster privileges
+    //todo check logger for unmigratable privileges
+    //todo index & cluster privileges
+    //todo check for logger warning of empty roles file
+    //todo check for logger warning of empty Alias action group
+    //todo check for logger warning of empty Datastream action group
+    //todo check for logger warning of empty Tenant action group
+}
