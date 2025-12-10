c957829b0a42b33ff67037e7de1319b29cd3e8f4
THarries <timonharries@googlemail.com>
2025-12-10 14:34:07 +0100

    removed todos

--- src/test/java/com/floragunn/searchguard/sgctl/config/migrator/RoleMigratorTest.java
+++ src/test/java/com/floragunn/searchguard/sgctl/config/migrator/RoleMigratorTest.java
@@ -47,13 +47,10 @@ public class RoleMigratorTest {
 
   //todo check correct replacement
   //todo check removal of unmigratable privileges
-  //todo index & cluster privileges
+  //todo index privileges
   //todo check logger for unmigratable privileges
-  //todo logger for index & cluster privileges
   //todo check for logger warning of empty roles file
-  //todo check for logger warning of empty Alias action group
-  //todo check for logger warning of empty Datastream action group
-  //todo check for logger warning of empty Tenant action group
+
   @Test
   public void testMigration() throws IOException, SgctlException {
     String resourcePath = "/xpack_migrate/roles/roles_test_cases_2.yml";
