1d829be1452ba7922471b3c5a97be9347b33ef25
TorkeTechnologies <support@torketechnologies.com>
2026-01-14 20:52:17 +0100

    Fixed tests that were broken by changes to the MigrationReport

--- src/test/java/com/floragunn/searchguard/sgctl/util/mapping/MigrationReportTest.java
+++ src/test/java/com/floragunn/searchguard/sgctl/util/mapping/MigrationReportTest.java
@@ -168,8 +168,6 @@ class MigrationReportTest {
      * Verifies report output includes headers and sections for populated entries.
      */
     @Test
-    @Disabled
-    //FIXME: Changes to MigrationReport broke this test
     void printReportIncludesHeaderAndSections() {
         report.addMigrated("file1", "old", "new");
         report.addWarning("file1", "w1", "Check");
