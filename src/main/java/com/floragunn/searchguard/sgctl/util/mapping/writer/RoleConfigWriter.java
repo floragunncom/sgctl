300b506a040df3d88a8f61e3d2a01f02f60eaffd
TorkeTechnologies <support@torketechnologies.com>
2026-01-17 15:01:07 +0100

    Fixed wrong names of dls and fls fields

--- src/main/java/com/floragunn/searchguard/sgctl/util/mapping/writer/RoleConfigWriter.java
+++ src/main/java/com/floragunn/searchguard/sgctl/util/mapping/writer/RoleConfigWriter.java
@@ -559,10 +559,10 @@ public class RoleConfigWriter implements Document<RoleConfigWriter> {
                 contents.put("index_patterns", indexPatterns);
                 contents.put("allowed_actions", allowedActions);
                 if (dls != null) {
-                    contents.put("_dls_", dls);
+                    contents.put("dls", dls);
                 }
                 if (fls != null && !fls.isEmpty()) {
-                    contents.put("_fls_", fls);
+                    contents.put("fls", fls);
                 }
                 return contents;
             }
